36a357e825600a9c6ebd5b86cff1eec8d8ed9adb
Lorenzo Mattei <lore.mattei@gmail.com>
2019-05-02 12:38:43 +0200

    Fix merge conflicts

--- lib/fastlane/plugin/wpmreleasetoolkit.rb
+++ lib/fastlane/plugin/wpmreleasetoolkit.rb
@@ -4,8 +4,7 @@ module Fastlane
   module Wpmreleasetoolkit
     # Return all .rb files inside the "actions" and "helper" directory
     def self.all_classes
-      Dir[File.expand_path('**/{actions,helper,actions/configure,actions/android,helper/android}/*.rb', File.dirname(__FILE__))]
-      Dir[File.expand_path('**/{actions,helper,actions/configure,actions/ios,helper/ios}/*.rb', File.dirname(__FILE__))]
+      Dir[File.expand_path('**/{actions,helper,actions/configure,actions/android,helper/android,actions/ios,helper/ios}/*.rb', File.dirname(__FILE__))]
     end
   end
 end
