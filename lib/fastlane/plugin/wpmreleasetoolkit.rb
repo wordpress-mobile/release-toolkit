403fc79281c21caabfa5da9afea439146fc8b64b
Lorenzo Mattei <lore.mattei@gmail.com>
2019-08-16 13:59:33 +0200

    Include all subfolders

--- lib/fastlane/plugin/wpmreleasetoolkit.rb
+++ lib/fastlane/plugin/wpmreleasetoolkit.rb
@@ -4,7 +4,7 @@ module Fastlane
   module Wpmreleasetoolkit
     # Return all .rb files inside the "actions" and "helper" directory
     def self.all_classes
-      Dir[File.expand_path('**/{actions,helper,actions/common,actions/configure,actions/android,helper/android,actions/ios,helper/ios}/*.rb', File.dirname(__FILE__))]
+      Dir[File.expand_path('**/{actions,helper}/**/*.rb', File.dirname(__FILE__))]
     end
   end
 end
