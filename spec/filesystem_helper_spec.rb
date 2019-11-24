e5ba0d062b7d95bbd9d8c38acaa1056685145482
Raven Stewart <raven.shiao.stewart@gmail.com>
2019-11-24 10:59:20 -0600

    put some duplicate code into functions in filesystem_helper_spec

--- spec/filesystem_helper_spec.rb
+++ spec/filesystem_helper_spec.rb
@@ -16,10 +16,7 @@ describe Fastlane::Helper::FilesystemHelper do
 it 'delete_files deletes all files based on given criteria' do
     files_match = ["file_match_1", "file_match_2", "file_match_3", "file_match_4"]
 
-    # add files to temp directory
-    files_match.each do |file|
-      File.new(file, "w")
-    end
+    create_files(files_match)
 
     files_deleted = Fastlane::Helper::FilesystemHelper.delete_files("file_match_?", false)
 
@@ -27,10 +24,7 @@ it 'delete_files deletes all files based on given criteria' do
     expect(files_deleted.sort).to eq files_match.sort
 
     # verify matching files were deleted
-    files_match.each do |file|
-      expect(File.exist?(file)).to be false
-    end
-
+    expect(check_files_exist(files_match, false)).to be true
   end
 
   it 'delete_files deletes only files based on given criteria' do
@@ -38,12 +32,8 @@ it 'delete_files deletes all files based on given criteria' do
     files_no_match = ["file_no_match_1", "file_no_match_2"]
 
     # add files to temp directory
-    files_match.each do |file|
-      File.new(file, "w")
-    end
-    files_no_match.each do |file|
-      File.new(file, "w")
-    end
+    create_files(files_match)
+    create_files(files_no_match)
 
     files_deleted = Fastlane::Helper::FilesystemHelper.delete_files("file_match_?", false)
 
@@ -51,23 +41,15 @@ it 'delete_files deletes all files based on given criteria' do
     expect(files_deleted.sort).to eq files_match.sort
 
     # verify matching files were deleted
-    files_match.each do |file|
-      expect(File.exist?(file)).to be false
-    end
-
-    # verify non-matching files were not deleted
-    files_no_match.each do |file|
-      expect(File.exist?(file)).to be true
-    end
+    expect(check_files_exist(files_match, false)).to be true
+    expect(check_files_exist(files_no_match, true)).to be true
   end
 
   it 'delete_files does not delete files when no files match given criteria' do
     files_no_match = ["file_no_match_1", "file_no_match_2"]
 
     # add files to temp directory
-    files_no_match.each do |file|
-      File.new(file, "w")
-    end
+    create_files(files_no_match)
 
     files_deleted = Fastlane::Helper::FilesystemHelper.delete_files("file_match_?", false)
 
@@ -75,9 +57,7 @@ it 'delete_files deletes all files based on given criteria' do
     expect(files_deleted.empty?).to be true
 
     # verify non-matching files were not deleted
-    files_no_match.each do |file|
-      expect(File.exist?(file)).to be true
-    end
+    expect(check_files_exist(files_no_match, true)).to be true
   end
 
   it 'delete_files results are empty when no files exist' do
@@ -86,4 +66,19 @@ it 'delete_files deletes all files based on given criteria' do
     # verify function return value
     expect(files_deleted.empty?).to be true
   end
+end
+
+def create_files(files_to_create)
+  files_to_create.each do |file|
+    File.new(file, "w")
+  end
+end
+
+def check_files_exist(files_to_check, exist)
+  files_to_check.each do |file|
+    if File.exist?(file) != exist then
+      return false
+    end
+  end
+  return true
 end
