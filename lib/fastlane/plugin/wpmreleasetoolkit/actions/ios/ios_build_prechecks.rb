767dab193379c66b5403dffeb32e9ff095ca5100
Lorenzo Mattei <lore.mattei@gmail.com>
2020-03-24 12:32:55 +0100

    Switch to is_ci action

--- lib/fastlane/plugin/wpmreleasetoolkit/actions/ios/ios_build_prechecks.rb
+++ lib/fastlane/plugin/wpmreleasetoolkit/actions/ios/ios_build_prechecks.rb
@@ -18,7 +18,7 @@ module Fastlane
         end
 
         # Check local repo status
-        other_action.ensure_git_status_clean() unless params[:run_on_ci]
+        other_action.ensure_git_status_clean() unless other_action.is_ci()
       end
 
       #####################################################
@@ -54,12 +54,7 @@ module Fastlane
                                           env_name: "FL_IOS_BUILD_PRECHECKS_INTERNAL_SV_BUILD",
                                           description: "True if this is for an internal build that follows the same versioning of the external",
                                           is_string: false, 
-                                          default_value: false), 
-          FastlaneCore::ConfigItem.new(key: :run_on_ci,
-                                          env_name: "FL_IOS_BUILD_PRECHECKS_RUN_ON_CI",
-                                          description: "True if running on CI",
-                                          is_string: false, 
-                                          default_value: false), 
+                                          default_value: false) 
         ]
       end
 
