f853850d917212c81c9f733885c43059a8e41b20
Lorenzo Mattei <lore.mattei@gmail.com>
2020-06-30 12:26:16 +0200

    Fix char count

--- lib/fastlane/plugin/wpmreleasetoolkit/helper/metadata_download_helper.rb
+++ lib/fastlane/plugin/wpmreleasetoolkit/helper/metadata_download_helper.rb
@@ -70,7 +70,7 @@ module Fastlane
       end
 
       def update_key(target_locale, key, file, data, msg)
-        message_len = msg.to_s.length - 3 
+        message_len = msg.to_s.length - 4 # Don't count JSON delimiters.  
         if (data.key?(:max_size)) && (data[:max_size] != 0) && ((message_len) > data[:max_size]) then
           if (data.key?(:alternate_key)) then
             UI.message("#{target_locale} translation for #{key} exceeds maximum length (#{message_len}). Switching to the alternate translation.")
