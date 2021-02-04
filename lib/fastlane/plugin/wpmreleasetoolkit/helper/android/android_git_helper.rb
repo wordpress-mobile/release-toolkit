289369ce19dc2f9926981e5fc2ad3bca43e1b3b0
Olivier Halligon <olivier.halligon@automattic.com>
2021-02-04 22:27:42 +0100

    move update_metadata for consistency with iOS

--- lib/fastlane/plugin/wpmreleasetoolkit/helper/android/android_git_helper.rb
+++ lib/fastlane/plugin/wpmreleasetoolkit/helper/android/android_git_helper.rb
@@ -2,16 +2,6 @@ module Fastlane
   module Helper
     module Android
       module GitHelper
-        def self.update_metadata(validate_translation_command)
-          Action.sh("./tools/update-translations.sh")
-          Action.sh("git submodule update --init --recursive")
-          Action.sh("./gradlew #{validate_translation_command}")
-          Action.sh("git add #{ENV["PROJECT_ROOT_FOLDER"]}#{ENV["PROJECT_NAME"]}/src/main/res")
-          Action.sh("git diff-index --quiet HEAD || git commit -m \"Updates translations\"")
-
-          Action.sh("git push origin HEAD")
-        end
-
         # Commit and push the files that are modified when we bump version numbers on an iOS project
         #
         # This typically commits and pushes the `build.gradle` file inside the project subfolder.
@@ -25,6 +15,16 @@ module Fastlane
             files: File.join(ENV["PROJECT_ROOT_FOLDER"], ENV["PROJECT_NAME"], "build.gradle"),
             push: true
           )
+        end
+
+        def self.update_metadata(validate_translation_command)
+          Action.sh("./tools/update-translations.sh")
+          Action.sh("git submodule update --init --recursive")
+          Action.sh("./gradlew #{validate_translation_command}")
+          Action.sh("git add #{ENV["PROJECT_ROOT_FOLDER"]}#{ENV["PROJECT_NAME"]}/src/main/res")
+          Action.sh("git diff-index --quiet HEAD || git commit -m \"Updates translations\"")
+
+          Action.sh("git push origin HEAD")
         end
       end
     end
