15c24009aad35c0ac87c0430c85c8eaf5fe7e9ae
Olivier Halligon <olivier.halligon@automattic.com>
2021-02-11 16:27:30 +0100

    fix ext/drawText/extconf.rb path constructors

--- ext/drawText/extconf.rb
+++ ext/drawText/extconf.rb
@@ -4,10 +4,10 @@ require 'os'
 
 drawTextDirectory = File.dirname(File.absolute_path(__FILE__))
 compilationDirectory = Dir.pwd
-libDirectory = File.dirname(File.dirname(drawTextDirectory)) + "/lib"
+libDirectory = File.join(File.dirname(File.dirname(drawTextDirectory)), '/lib')
 
 # Copy the makefile required for compilation
-FileUtils.cp(drawTextDirectory + "/makefile.example", compilationDirectory + "/Makefile")
+FileUtils.cp(File.join(drawTextDirectory, 'makefile.example'), File.join(compilationDirectory, 'Makefile'))
 
 # Only compile drawText on macOS
 unless OS.mac? then
@@ -19,15 +19,21 @@ find_executable('swift')
 find_executable('xcodebuild')
 
 
-system("xcodebuild -project #{drawTextDirectory}/drawText.xcodeproj/ -scheme drawText -derivedDataPath #{compilationDirectory} -configuration RELEASE")
+system(
+  'xcodebuild',
+  '-project', File.join(drawTextDirectory, 'drawText.xcodeproj'),
+  '-scheme', 'drawText',
+  '-derivedDataPath', compilationDirectory,
+  '-configuration', 'RELEASE'
+)
 
-compiledPath = compilationDirectory + "/Build/Products/Release/drawText"
-destinationPath = libDirectory + "/drawText"
+compiledPath = File.join(compilationDirectory, 'Build', 'Products', 'Release', 'drawText')
+destinationPath = File.join(libDirectory, 'drawText')
 
 # Delete and overwrite the binary
 FileUtils.rm_rf(destinationPath)
 FileUtils.cp(compiledPath, destinationPath)
 
 # Delete and overwrite the bundle file
-FileUtils.rm_rf(libDirectory + "/drawText.bundle")
+FileUtils.rm_rf(File.join(libDirectory, 'drawText.bundle'))
 FileUtils.touch(File.join(compilationDirectory, 'drawText.' + RbConfig::CONFIG['DLEXT']))
