dc02dd3e945d1763c66fcc92ccb12c71878c5f09
Lorenzo Mattei <lore.mattei@gmail.com>
2021-03-10 11:35:02 +0100

    Improve tests

--- spec/android_localize_helper_spec.rb
+++ spec/android_localize_helper_spec.rb
@@ -8,29 +8,35 @@ describe Fastlane::Helper::Android::LocalizeHelper do
     end
 
     it 'returns nil when the key is not present' do
-      test_file_content = 'my-test-key-foo = \'foo\''
-      test_file_content += 'my-test-key-bad = \'bad\''
-      test_file_content += 'my-test-key = \'my_test_value\''
+      test_file_content = <<~CONTENT
+        my-test-key-foo = 'foo'
+        my-test-key-bad = 'bad'
+        my-test-key = 'my_test_value'
+      CONTENT
 
       allow(File).to receive(:exists?).and_return(true)
       allow(File).to receive(:open).with('./build.gradle', 'r').and_yield(StringIO.new(test_file_content))
-      expect(subject.get_library_version_from_gradle_config(import_key: 'my-test-key2')).to be_nil
+      expect(subject.get_library_version_from_gradle_config(import_key: 'my-test-key-b')).to be_nil
     end
 
     it 'returns the key content when the key is present' do
-      test_file_content = 'my-test-key-foo = \'foo\''
-      test_file_content += 'my-test-key-bad = \'bad\''
-      test_file_content += 'my-test-key = \'my_test_value\''
+      test_file_content = <<~CONTENT
+        my-test-key-foo = 'foo'
+        my-test-key-bad = 'bad'
+        my-test-key = 'my_test_value'
+      CONTENT
 
       allow(File).to receive(:exists?).and_return(true)
       allow(File).to receive(:open).with('./build.gradle', 'r').and_yield(StringIO.new(test_file_content))
       expect(subject.get_library_version_from_gradle_config(import_key: 'my-test-key')).to eq('my_test_value')
 
       # Make sure it handles double quotes
-      test_file_content = 'my-test-key-foo = \'foo\''
-      test_file_content += 'some-other-content = "dummy"'
-      test_file_content += 'my-test-key = "my_test_value"'
-      
+      test_file_content = <<~CONTENT
+        my-test-key-foo = 'foo'
+        my-test-key-bad = "bad"
+        my-test-key = "my_test_value"
+      CONTENT
+
       allow(File).to receive(:exists?).and_return(true)
       allow(File).to receive(:open).with('./build.gradle', 'r').and_yield(StringIO.new(test_file_content))
       expect(subject.get_library_version_from_gradle_config(import_key: 'my-test-key')).to eq('my_test_value')
