07fb871d7316567a4ea628afdcf8bf8f81549bac
Lorenzo Mattei <lore.mattei@gmail.com>
2021-03-16 11:37:12 +0100

    Remove unnecessary calls

--- lib/fastlane/plugin/wpmreleasetoolkit/helper/github_helper.rb
+++ lib/fastlane/plugin/wpmreleasetoolkit/helper/github_helper.rb
@@ -82,9 +82,8 @@ module Fastlane
       # @return [String] The path of the downloaded file, or nil if something went wrong
       #
       def self.download_file_from_release(repository:, release:, file_path:, download_folder:)
-        repository = repository.delete_prefix('/').chomp('/').concat('/')
-        file_path = file_path.delete_prefix('/').chomp('/').concat('/')
-        release = release.concat('/')
+        repository = repository.delete_prefix('/')
+        file_path = file_path.delete_prefix('/')
         file_name = File.basename(file_path)
         download_path = File.join(download_folder, file_name)
 
