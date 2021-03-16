30846bb923c9e80b81796fbfacac484858d6e273
Lorenzo Mattei <lore.mattei@gmail.com>
2021-03-16 11:57:03 +0100

    Improve tests

--- spec/android_localize_helper_spec.rb
+++ spec/android_localize_helper_spec.rb
@@ -17,13 +17,14 @@ describe Fastlane::Helper::Android::LocalizeHelper do
       allow(File).to receive(:exists?).and_return(true)
       allow(File).to receive(:open).with('./build.gradle', 'r').and_yield(StringIO.new(test_file_content))
       expect(subject.get_library_version_from_gradle_config(import_key: 'my-test-key-b')).to be_nil
+      expect(subject.get_library_version_from_gradle_config(import_key: 'test-key')).to be_nil
     end
 
     it 'returns the key content when the key is present' do
       test_file_content = <<~CONTENT
         my-test-key-foo = 'foo'
         my-test-key-bad = 'bad'
-        my-test-key = 'my_test_value'
+        my-test-key =  'my_test_value'
       CONTENT
 
       allow(File).to receive(:exists?).and_return(true)
