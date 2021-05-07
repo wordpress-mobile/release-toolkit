c88cc2488a0e96ccfb727649c1266c80681e9e27
Javon Davis <JavonDavis@users.noreply.github.com>
2021-05-07 11:52:56 -0500

    include app in available options

--- lib/fastlane/plugin/wpmreleasetoolkit/actions/android/android_get_release_version.rb
+++ lib/fastlane/plugin/wpmreleasetoolkit/actions/android/android_get_release_version.rb
@@ -23,6 +23,13 @@ module Fastlane
 
       def self.available_options
         # Define all options your action supports.
+        [
+          FastlaneCore::ConfigItem.new(key: :app,
+                                       env_name: 'APP',
+                                       description: 'The app to get the release version for',
+                                       is_string: true, # true: verifies the input is a string, false: every kind of value
+                                       default_value: 'wordpress'), # the default value if the user didn't provide one
+        ]
       end
 
       def self.output
