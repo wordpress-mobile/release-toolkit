7d2334f4ed7153a51e9c3d2267073969a212f17e
Olivier Halligon <olivier.halligon@automattic.com>
2021-05-10 20:22:10 +0200

    Fix test after autocorrect exists?/exist?

--- spec/android_version_helper_spec.rb
+++ spec/android_version_helper_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper.rb'
 describe Fastlane::Helper::Android::VersionHelper do
   describe 'get_library_version_from_gradle_config' do
     it 'returns nil when gradle file is not present' do
-      allow(File).to receive(:exists?).and_return(false)
+      allow(File).to receive(:exist?).and_return(false)
       expect(subject.get_library_version_from_gradle_config(import_key: 'my-test-key')).to be_nil
     end
 
@@ -14,7 +14,7 @@ describe Fastlane::Helper::Android::VersionHelper do
         my-test-key = 'my_test_value'
       CONTENT
 
-      allow(File).to receive(:exists?).and_return(true)
+      allow(File).to receive(:exist?).and_return(true)
       allow(File).to receive(:open).with('./build.gradle', 'r').and_yield(StringIO.new(test_file_content))
       expect(subject.get_library_version_from_gradle_config(import_key: 'my-test-key-b')).to be_nil
       expect(subject.get_library_version_from_gradle_config(import_key: 'test-key')).to be_nil
@@ -27,7 +27,7 @@ describe Fastlane::Helper::Android::VersionHelper do
         my-test-key =  'my_test_value'
       CONTENT
 
-      allow(File).to receive(:exists?).and_return(true)
+      allow(File).to receive(:exist?).and_return(true)
       allow(File).to receive(:open).with('./build.gradle', 'r').and_yield(StringIO.new(test_file_content))
       expect(subject.get_library_version_from_gradle_config(import_key: 'my-test-key')).to eq('my_test_value')
 
@@ -38,7 +38,7 @@ describe Fastlane::Helper::Android::VersionHelper do
         my-test-key = "my_test_value"
       CONTENT
 
-      allow(File).to receive(:exists?).and_return(true)
+      allow(File).to receive(:exist?).and_return(true)
       allow(File).to receive(:open).with('./build.gradle', 'r').and_yield(StringIO.new(test_file_content))
       expect(subject.get_library_version_from_gradle_config(import_key: 'my-test-key')).to eq('my_test_value')
 
@@ -50,7 +50,7 @@ describe Fastlane::Helper::Android::VersionHelper do
         ext..my_test_key_double = 'foo'
       CONTENT
 
-      allow(File).to receive(:exists?).and_return(true)
+      allow(File).to receive(:exist?).and_return(true)
       allow(File).to receive(:open).with('./build.gradle', 'r').and_yield(StringIO.new(test_file_content))
       expect(subject.get_library_version_from_gradle_config(import_key: 'my-test-key')).to eq('my_test_value')
       expect(subject.get_library_version_from_gradle_config(import_key: 'my_test_key_double')).to be_nil
@@ -62,7 +62,7 @@ describe Fastlane::Helper::Android::VersionHelper do
               ext.my-test-key = 'my_test_value'
       CONTENT
 
-      allow(File).to receive(:exists?).and_return(true)
+      allow(File).to receive(:exist?).and_return(true)
       allow(File).to receive(:open).with('./build.gradle', 'r').and_yield(StringIO.new(test_file_content))
       expect(subject.get_library_version_from_gradle_config(import_key: 'my-test-key')).to eq('my_test_value')
     end
