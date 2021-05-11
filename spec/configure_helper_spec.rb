419ab1e1ed9786081c4423338562ab2b81c23c41
Gio Lodi <giovanni.lodi42@gmail.com>
2021-05-11 14:44:53 +1000

    Run ConfigureHelper spec in a tmp folder to avoid making repo dirty

--- spec/configure_helper_spec.rb
+++ spec/configure_helper_spec.rb
@@ -1,6 +1,25 @@
 require 'spec_helper.rb'
 
+shared_context 'with temp dir' do
+  before do
+    @tmpdir_path = Dir.mktmpdir
+    @pwd_before_spec_run = Dir.pwd
+    Dir.chdir(tmpdir_path)
+  end
+
+  after do
+    Dir.chdir(pwd_before_spec_run)
+    puts "tmp path is #{tmpdir_path}"
+    FileUtils.rm_rf(tmpdir_path)
+  end
+
+  attr_reader :tmpdir_path
+  attr_reader :pwd_before_spec_run
+end
+
 describe Fastlane::Helper::ConfigureHelper do
+  include_context 'with temp dir'
+
   describe '#add_file' do
     let(:destination) { 'path/to/destination' }
 
@@ -8,6 +27,9 @@ describe Fastlane::Helper::ConfigureHelper do
       allow(Fastlane::Helper::GitHelper).to receive(:is_ignored?)
         .with(path: destination)
         .and_return(false)
+
+      allow(Fastlane::Helper::FilesystemHelper).to receive(:project_path)
+        .and_return(Pathname.new(tmpdir_path))
 
       expect(Fastlane::UI).to receive(:user_error!)
 
