d90cd4717eb228fc067a2b458baaff493809d598
Lorenzo Mattei <lore.mattei@gmail.com>
2021-06-24 15:02:18 +0200

    Improve the query

--- lib/fastlane/plugin/wpmreleasetoolkit/helper/glotpress_helper.rb
+++ lib/fastlane/plugin/wpmreleasetoolkit/helper/glotpress_helper.rb
@@ -26,9 +26,32 @@ module Fastlane
       # @return [Integer] The percentage of the translated strings.
       #
       def self.get_translation_status(data:, language_code:)
-        regex = "$\\s*<strong><a href=\".*\\/#{language_code}\\/default\\/\">.*<\\/strong>\\n.*\\n*.*\\n*.*<td class=\"stats percent\">([0-9]+)%<\/td>$"
+        # The status is parsed from Glotpress project page.
+        # The row can be identified by the language code and the progress is in the column identified by the class "stats percent".
+        # When the progress is above 90%, a special badge is added.
+        # Because of the way the HTML is organized, this regex matches content spawned on three or four lines
+        # Regex:
+        # ^           : start of a line
+        # \s*         : any space
+        # <strong><a href=".*\/#{language_code}\/default\/"> : This link contains the language code of that line in the HTML table, so it's a reliable match
+        # .*          : any character. The language name should be here, but it can be less reliable than the language code as a match
+        # <\/strong>  : tag closure
+        # \n          : new line
+        # (?:         : match the following. This starts the "morethan90" special badge, which we expect to exist zero or one times (see the closure of this part of the regex).
+        #       \s*         : any space
+        #       <span class="bubble morethan90"> : Start of the special badge
+        #       \d\d\d?%    : 2 or 3 digits and the percentage char
+        #       <\/span>\n  : Special badge closure and new line
+        # )?          : end of the "morethan90" special badge section. Expect this zero or one times.
+        # \s*<\/td>\n : column closure tag. Any space before of it are ok. Expect new line after it.
+        # \s*         : any space
+        # <td class="stats percent"> : This is the tag which can be used to extract the progress
+        # ([0-9]+)    : progress is the first group
+        # %<\/td>     : tag closure
+        regex = "^\\s*<strong><a href=\".*\\/#{language_code}\\/default\\/\">.*<\\/strong>\\n"
+        regex += "(?:\\s*<span class=\"bubble morethan90\">\\d\\d\\d?%<\\/span>\\n)?\\s*<\\/td>\\n\\s*<td class=\"stats percent\">([0-9]+)%<\\/td>$"
 
-        # 1. Grep the line with contains the required info.
+        # 1. Merge the array into a single string.
         # 2. Match the info and extract the value in group 1.
         # 3. Convert to integer.
         data.join("\n").match(/#{regex}/)[1].to_i
