dad59a7b768a59e14f2951d7155a086b27eb991c
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2021-07-23 14:44:26 -0600

    Update lib/fastlane/plugin/wpmreleasetoolkit/helper/filesystem_helper.rb
    Co-authored-by: Olivier Halligon <olivier.halligon@automattic.com>

--- lib/fastlane/plugin/wpmreleasetoolkit/helper/filesystem_helper.rb
+++ lib/fastlane/plugin/wpmreleasetoolkit/helper/filesystem_helper.rb
@@ -36,8 +36,9 @@ module Fastlane
         while continue
           child_filenames = dir.children.map! { |x| File.basename(x) }
 
-          # The first option is for development (where the .gemspec is present in the project root), the second is for production (where it is not)
-          if child_filenames.include?('fastlane-plugin-wpmreleasetoolkit.gemspec') || File.basename(dir).include?('fastlane-plugin-wpmreleasetoolkit')
+          # The first case is for development – where the `.gemspec` is present in the project root
+          # The second case is for production – where there's no `.gemspec`, but the root dir of the plugin is named `fastlane-plugin-wpmreleasetoolkit-{version}`.
+          if child_filenames.include?('fastlane-plugin-wpmreleasetoolkit.gemspec') || File.basename(dir).start_with?('fastlane-plugin-wpmreleasetoolkit')
             continue = false
           else
             dir = dir.parent
