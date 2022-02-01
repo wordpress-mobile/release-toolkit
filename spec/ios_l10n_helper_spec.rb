e9cd88a9b1dc029648187863c5d237416363113e
Olivier Halligon <olivier.halligon@automattic.com>
2022-02-01 14:28:57 +0100

    Fix typo again

--- spec/ios_l10n_helper_spec.rb
+++ spec/ios_l10n_helper_spec.rb
@@ -195,7 +195,7 @@ describe Fastlane::Helper::Ios::L10nHelper do
         Dir.mktmpdir('a8c-release-toolkit-tests-') do |tmp_dir|
           # Arrange
           # Note: in practice it seems that GlotPress's `.strings` exports are using UTF-8 (but served as `application/octet-stream`)
-          #       but it does not hurt to ensure the download to a file can work with UTF-16 ()and copy the binary stream verbatim)
+          #       but it does not hurt to ensure the download to a file can work with UTF-16 (and copy the binary stream verbatim)
           body = File.read(fixture('Localizable-utf16.strings'))
           stub = stub_request(:get, "#{gp_fake_url}/fr/default/export-translations?format=strings").to_return(body: body)
           dest = File.join(tmp_dir, 'export.strings')
