20686c60a9f4298eb2304da488ee639012df773f
Gio Lodi <giovanni.lodi42@gmail.com>
2022-02-25 16:46:36 +1100

    WIP, but much closer to working properly

--- spec/strings_file_validator_spec.rb
+++ spec/strings_file_validator_spec.rb
@@ -4,18 +4,26 @@ module StringsFileValidator
 end
 
 def extract_key(line:)
-  # The `gsub(\\./, '..')` call replaces any backslash-escape (\n, \r, \\, \",
-  # …) with 2 different characters that won't mess up our RegEx instead
-  key_range =
-    line
-    .gsub(/\\./, '..')
-    .match(/^\s*"([^"]*)"/)&.offset(1)
+  # Notice the [\S\s] which accounts for escaped characters, which are
+  # allowed according to the `.strings` spec.
+  #
+  # TODO: 👆 That's not accurate
+  #
+  # See
+  # https://developer.apple.com/library/archive/documentation/Cocoa/Conceptual/LoadingResources/Strings/Strings.html#//apple_ref/doc/uid/10000051i-CH6-SW13
+  # (Using Special Characters in String Resources)
+  regexp = /^\s*"([^"]*[\S\s]*[^"])"\s*=/
+  key_range_offset0 = line.match(regexp)&.offset(0)
+  key_range_offset1 = line.match(regexp)&.offset(1)
+  # puts "match: #{line.match(regexp)}"
+  # puts "range 0: #{key_range_offset0}"
+  # puts "other 1: #{key_range_offset1}"
 
-  return nil if key_range.nil?
+  return nil if key_range_offset0.nil?
 
-  # Return the same range of character than the one found, but from the
-  # original string (pre-backslash-escape-replacements)
-  return line[key_range[0]...key_range[1]]
+  # puts "key at offest 0: #{line[key_range_offset0[0]...key_range_offset0[1]]}"
+  # puts "key at offest 1: #{line[key_range_offset1[0]...key_range_offset1[1]]}"
+  line[key_range_offset1[0]...key_range_offset1[1]]
 end
 
 def find_duplicated_keys(file:)
@@ -29,6 +37,7 @@ def find_duplicated_keys(file:)
     # Check if that key was already encountered in the past
     existing = found_keys[key]
 
+    puts key
     duplicated_keys.append(key) unless existing.nil?
     puts "warning: key `#{key}` on line #{index + 1} is a duplicate of a similar key found on line #{existing + 1}" unless existing.nil?
 
@@ -94,10 +103,16 @@ describe StringsFileValidator do
       "\U0026 key" = "\U0025 is the & symbol";
       "unicode\U0020key" = "\U0020 is the space character";
       "\U0026 key" = "\U0025 is the & symbol";
-      /* Special case: \"%@\" can be tricky to detect in RegExp */
+      /* Special case: \"%@\" can be tricky to detect */
       "The Google account \"%@\" doesn't match any account on WordPress.com" = "localized translation here";
       /* A red herring that might be seen as duplicate to the previous one if our RegExp does not correctly account for escaped quotes */
       "The Google account \"%@\" is invalid" = "localized translation here";
+      /* Duplicated \"%@\" */
+      "key with \"%@\" character" = "localized translation here";
+      "key with \"%@\" character" = "localized translation here";
+      /* This is just a check for Gio's implementation. We might want to remove it? */
+      "key with trailing spaces " = "localized translation here";
+      "key with trailing spaces " = "localized translation here";
     PO
 
     expect(find_duplicated_keys(file: dummy_text)).to match_array [
