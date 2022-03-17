01ebad88433437a67c01e607d011da5ce79f745c
Olivier Halligon <olivier.halligon@automattic.com>
2022-03-17 18:13:43 +0100

    Cleanup `gem-push.sh`
    To make the name of the `.gem` file being build be deterministic (and avoid any risks of the shell picking up an old `*.gem` file by accident)

--- .buildkite/gem-push.sh
+++ .buildkite/gem-push.sh
@@ -1,12 +1,14 @@
 #!/bin/bash -eu
 
+GEM_NAME="fastlane-plugin-wpmreleasetoolkit"
+
 echo "--- :hammer: Build Gemspec"
-gem build fastlane-plugin-wpmreleasetoolkit.gemspec
+gem build "$GEM_NAME.gemspec" -o "$GEM_NAME.gem"
 
 echo "--- :sleuth_or_spy: Validate Gem Install"
-gem install --user-install fastlane-plugin-wpmreleasetoolkit-*.gem
+gem install --user-install "$GEM_NAME.gem"
 
 echo "--- :rubygems: Gem Push"
 echo ":rubygems_api_key: ${RUBYGEMS_API_KEY}" >>".gem-credentials"
 chmod 600 ".gem-credentials"
-gem push --config-file ".gem-credentials" fastlane-plugin-wpmreleasetoolkit-*.gem
+gem push --config-file ".gem-credentials" "$GEM_NAME.gem"
