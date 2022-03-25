434382663cabc36512bb7d948d9dbdd5603fd3f7
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2022-03-24 23:05:41 -0600

    Add next_rc_version action

---
+++ lib/fastlane/plugin/wpmreleasetoolkit/actions/versions/next_rc_version_action.rb
@@ -0,0 +1,73 @@
+module Fastlane
+  module Actions
+    class NextRcVersionAction < Action
+      def self.run(params)
+        require 'octokit'
+        require 'git'
+        require_relative '../../helper/version_helper'
+
+        client = Octokit::Client.new(access_token: params[:access_token])
+        client.auto_paginate = true
+
+        helper = Fastlane::Helper::VersionHelper.new(
+          github_client: client,
+          git: Git.open(Dir.pwd)
+        )
+
+        version = Fastlane::Helper::Version.create(params[:version])
+        next_version = helper.next_rc_for_version(version, repository: params[:project])
+
+        UI.message "Next RC Version is #{next_version.rc}"
+
+        next_version
+      end
+
+      #####################################################
+      # @!group Documentation
+      #####################################################
+
+      def self.description
+        'Return the next RC Version for this branch'
+      end
+
+      def self.available_options
+        [
+          FastlaneCore::ConfigItem.new(
+            key: :access_token,
+            env_name: 'GITHUB_TOKEN',
+            description: 'The GitHub token to use when querying GitHub',
+            type: String,
+            sensitive: true
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :version,
+            description: 'The current version',
+            type: String,
+            verify_block: proc { |v| UI.user_error!("Invalid version number: #{v}") if Fastlane::Helper::Version.create(v).nil? }
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :project,
+            description: 'The project slug (ex: `wordpress-mobile/wordpress-ios`)',
+            type: String
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :project_root,
+            env_name: 'PROJECT_ROOT_FOLDER',
+            description: 'The project root folder (that contains the .git directory)',
+            type: String,
+            default_value: Dir.pwd,
+            verify_block: proc { |v| UI.user_error!("Directory does not exist: #{v}") unless File.directory? v }
+          ),
+        ]
+      end
+
+      def self.authors
+        ['Automattic']
+      end
+
+      def self.is_supported?(platform)
+        true
+      end
+    end
+  end
+end
