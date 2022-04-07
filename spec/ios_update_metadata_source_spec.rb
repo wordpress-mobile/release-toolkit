24b13a76e88504d0a0f86d210050835fdfa8267c
Gio Lodi <giovanni.lodi42@gmail.com>
2022-04-07 13:47:12 +1000

    Add spec for `ios_update_metadata_source`
    I'm not even sure whether this is used or not, but for the moment I'm
    going to assume it is used somewhere.

---
+++ spec/ios_update_metadata_source_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+require 'shared_examples_for_update_metadata_source_action'
+
+describe Fastlane::Actions::IosUpdateMetadataSourceAction do
+  before {
+    # This works around the `ensure_git_status_clean` call within the action.
+    # It would be quite cumbersome to iterate on the code if we had to commit
+    # every change _before_ running the tests...
+    allow(Fastlane::Actions::EnsureGitStatusCleanAction).to receive(:run)
+  }
+
+  include_examples 'update_metadata_source_action', whats_new_fails: false
+end
