29c89f411ac8a09692d94efed2a1f205c91260d0
Gio Lodi <giovanni.lodi42@gmail.com>
2022-04-11 14:19:54 +1000

    Address `ios_strings_file_validation_helper_spec.rb` RuboCop violations

--- spec/ios_strings_file_validation_helper_spec.rb
+++ spec/ios_strings_file_validation_helper_spec.rb
@@ -15,22 +15,24 @@ describe Fastlane::Helper::Ios::StringsFileValidationHelper do
     it 'returns them in an array' do
       input = File.join(test_data_dir, 'file-with-duplicated-keys.strings')
 
-      expect(described_class.find_duplicated_keys(file: input)).to eq({
-        'dup1' => [30, 31],
-        'dup2' => [33, 35],
-        'dup3' => [36, 39],
-        'dup4' => [41, 42],
-        '\U0025 key' => [49, 50],
-        '\U0026 key' => [52, 54],
-        'key with \"%@\" character' => [60, 61],
-        'key with \"%@\" but diff translations' => [63, 64],
-        'key with multiple \"%@\" escapes \":)\" in it' => [66, 68],
-        'Login to a \"%@\" account' => [67, 69],
-        'key with trailing spaces ' => [76, 77],
-        'key with \"%@\" and = character' => [71, 72],
-        'key with \"%@\" character and equal in translation' => [73, 74],
-        'key repeated more than twice' => [79, 80, 81],
-      })
+      expect(described_class.find_duplicated_keys(file: input)).to eq(
+        {
+          'dup1' => [30, 31],
+          'dup2' => [33, 35],
+          'dup3' => [36, 39],
+          'dup4' => [41, 42],
+          '\U0025 key' => [49, 50],
+          '\U0026 key' => [52, 54],
+          'key with \"%@\" character' => [60, 61],
+          'key with \"%@\" but diff translations' => [63, 64],
+          'key with multiple \"%@\" escapes \":)\" in it' => [66, 68],
+          'Login to a \"%@\" account' => [67, 69],
+          'key with trailing spaces ' => [76, 77],
+          'key with \"%@\" and = character' => [71, 72],
+          'key with \"%@\" character and equal in translation' => [73, 74],
+          'key repeated more than twice' => [79, 80, 81]
+        }
+      )
     end
   end
 
