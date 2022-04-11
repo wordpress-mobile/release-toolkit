030bb70f1b86bde9452c0f28b4c2145db9e5ce67
Gio Lodi <giovanni.lodi42@gmail.com>
2022-04-11 20:29:31 +1000

    Address RuboCop violation: replace `before {}` with `before do end`

--- spec/ios_update_metadata_source_spec.rb
+++ spec/ios_update_metadata_source_spec.rb
@@ -2,12 +2,12 @@ require 'spec_helper'
 require 'shared_examples_for_update_metadata_source_action'
 
 describe Fastlane::Actions::IosUpdateMetadataSourceAction do
-  before {
+  before do
     # This works around the `ensure_git_status_clean` call within the action.
     # It would be quite cumbersome to iterate on the code if we had to commit
     # every change _before_ running the tests...
     allow(Fastlane::Actions::EnsureGitStatusCleanAction).to receive(:run)
-  }
+  end
 
   include_examples 'update_metadata_source_action', whats_new_fails: false
 end
