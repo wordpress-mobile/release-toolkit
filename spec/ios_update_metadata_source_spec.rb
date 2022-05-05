fb7f9546d14f7b6e4b7614f2785bdd3e3bb84342
Gio Lodi <giovanni.lodi42@gmail.com>
2022-05-05 20:33:43 +1000

    Clarify comment on stub in `ios_update_metadata_source_spec.rb`

--- spec/ios_update_metadata_source_spec.rb
+++ spec/ios_update_metadata_source_spec.rb
@@ -4,8 +4,13 @@ require 'shared_examples_for_update_metadata_source_action'
 describe Fastlane::Actions::IosUpdateMetadataSourceAction do
   before do
     # This works around the `ensure_git_status_clean` call within the action.
-    # It would be quite cumbersome to iterate on the code if we had to commit
-    # every change _before_ running the tests...
+    #
+    # We can't easily remove the need to stub the call here without removing the check in the action.
+    # In the tests, we move into a temp folder, but then create files in that directory.
+    # So, `ensure_git_status_clean` will fail anyway, unless we add more cruft to the tests.
+    #
+    # See also conversation in
+    # https://github.com/wordpress-mobile/release-toolkit/pull/352
     allow(Fastlane::Actions::EnsureGitStatusCleanAction).to receive(:run)
   end
 
