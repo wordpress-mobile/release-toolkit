12c74945f868f170817f7f53111fa6d1e40bd728
Olivier Halligon <olivier.halligon@automattic.com>
2022-05-09 18:46:25 +0200

    Introduce AppSizeMetricsHelper
    To help build a payload for the grouped-metrics API

---
+++ lib/fastlane/plugin/wpmreleasetoolkit/helper/app_size_metrics_helper.rb
@@ -0,0 +1,95 @@
+require 'json'
+require 'net/http'
+require 'zlib'
+
+module Fastlane
+  module WPMRT
+    # A helper class to build an App Size Metrics payload and send it to a server (or write it to disk)
+    #
+    # The payload generated (and sent) by this helper conforms to the API for grouped metrics described in
+    # https://github.com/Automattic/apps-metrics
+    #
+    class AppSizeMetricsHelper
+      # @param [Hash] group_meta Metadata common to all the metrics. Can be any arbitrary set of key/value pairs.
+      #
+      def initialize(group_meta = {})
+        self.meta = group_meta
+        @metrics = []
+      end
+
+      # Sets the metadata common to the whole group of metrics in the payload being built by this helper instance
+      #
+      # @param [Hash] hash The metadata common to all the metrics of the payload built by that helper instance. Can be any arbitrary set of key/value pairs
+      #
+      def meta=(hash)
+        @meta = (hash.compact || {}).map { |key, value| { name: key.to_s, value: value } }
+      end
+
+      # Adds a single metric to the group of metrics
+      #
+      # @param [String] name The metric name
+      # @param [Integer] value The metric value
+      # @param [Hash] meta The arbitrary dictionary of metadata to associate to that metric entry
+      #
+      def add_metric(name:, value:, meta: nil)
+        metric = { name: name, value: value }
+        meta = (meta || {}).compact # Remove nil values if any
+        metric[:meta] = meta.map { |meta_key, meta_value| { name: meta_key.to_s, value: meta_value } } unless meta.empty?
+        @metrics.append(metric)
+      end
+
+      def to_h
+        {
+          meta: @meta,
+          metrics: @metrics
+        }
+      end
+
+      # Send the metrics to the given App Metrics endpoint.
+      #
+      # Must conform to the API described in https://github.com/Automattic/apps-metrics/wiki/Queue-Group-of-Metrics
+      #
+      # @param [String,URI] base_url The base URL of the App Metrics service
+      # @param [String] api_token The API bearer token to use to register the metric.
+      # @return [Integer] the HTTP response code
+      #
+      def send_metrics(base_url:, api_token:, use_gzip: true)
+        uri = URI(base_url)
+        json_payload = use_gzip ? Zlib.gzip(to_h.to_json) : to_h.to_json
+
+        # Allow using a `file:` URI for debugging
+        if uri.is_a?(URI::File)
+          UI.message("Writing metrics payload to file #{uri.path} (instead of sending it to a server)")
+          File.write(uri.path, json_payload)
+          return 201 # To make it easy at call site to check for pseudo-status code 200 even in non-HTTP cases
+        end
+
+        UI.message("Sending metrics to #{uri}...")
+        headers = {
+          Authorization: "Bearer #{api_token}",
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        }
+        headers[:'Content-Encoding'] = 'gzip' if use_gzip
+
+        request = Net::HTTP::Post.new(uri, headers)
+        request.body = json_payload
+
+        response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
+          http.request(request)
+        end
+
+        if response.is_a?(Net::HTTPSuccess)
+          UI.message("Metrics sent. (#{response.code} #{response.message})")
+        else
+          UI.error("Metrics failed to send. Received: #{response.code} #{response.message}")
+          UI.message("Request was #{request.method} to #{request.uri}")
+          UI.message("Request headers were: #{headers}")
+          UI.message("Request body was #{request.body.length} bytes")
+          UI.message("Response was #{response.body}")
+        end
+        response.code.to_i
+      end
+    end
+  end
+end
---
+++ spec/app_size_metrics_helper_spec.rb
@@ -0,0 +1,153 @@
+require_relative './spec_helper'
+
+describe Fastlane::WPMRT::AppSizeMetricsHelper do
+  describe '#to_h' do
+    it 'generates the right payload from raw data' do
+      metrics_helper = described_class.new({
+                                             'Group Metadata 1': 'Group Value 1',
+                                             'Group Metadata 2': 'Group Value 2'
+                                           })
+      metrics_helper.add_metric(name: 'Metric 1', value: 12_345, meta: { m1a: 'Metric 1 Metadata A' })
+      metrics_helper.add_metric(name: 'Metric 2', value: 67_890)
+      metrics_helper.add_metric(name: 'Metric 3', value: 13_579, meta: { m3a: 'Metric 3 Metadata A', m3b: 'Metric 3 Metadata B' })
+
+      expected_hash = {
+        meta: [
+          { name: 'Group Metadata 1', value: 'Group Value 1' },
+          { name: 'Group Metadata 2', value: 'Group Value 2' },
+        ],
+        metrics: [
+          { name: 'Metric 1', value: 12_345, meta: [{ name: 'm1a', value: 'Metric 1 Metadata A' }] },
+          { name: 'Metric 2', value: 67_890 },
+          { name: 'Metric 3', value: 13_579, meta: [{ name: 'm3a', value: 'Metric 3 Metadata A' }, { name: 'm3b', value: 'Metric 3 Metadata B' }] },
+        ]
+      }
+      expect(metrics_helper.to_h).to eq(expected_hash)
+    end
+
+    it 'removes `nil` values in metadata' do
+      metrics_helper = described_class.new({
+                                             'Group Metadata 1': 'Group Value 1',
+                                             'Group Metadata 2': nil,
+                                             'Group Metadata 3': 'Group Value 3'
+                                           })
+      metrics_helper.add_metric(name: 'Metric 1', value: 12_345, meta: { m1a: 'Metric 1 Metadata A', m1b: nil, m1c: 'Metric 1 Metadata C' })
+      metrics_helper.add_metric(name: 'Metric 2', value: 67_890, meta: { m2a: nil })
+      metrics_helper.add_metric(name: 'Metric 3', value: 13_579, meta: { m3a: 'Metric 3 Metadata A', m3b: 'Metric 3 Metadata B' })
+
+      expected_hash = {
+        meta: [
+          { name: 'Group Metadata 1', value: 'Group Value 1' },
+          { name: 'Group Metadata 3', value: 'Group Value 3' },
+        ],
+        metrics: [
+          { name: 'Metric 1', value: 12_345, meta: [{ name: 'm1a', value: 'Metric 1 Metadata A' }, { name: 'm1c', value: 'Metric 1 Metadata C' }] },
+          { name: 'Metric 2', value: 67_890 },
+          { name: 'Metric 3', value: 13_579, meta: [{ name: 'm3a', value: 'Metric 3 Metadata A' }, { name: 'm3b', value: 'Metric 3 Metadata B' }] },
+        ]
+      }
+      expect(metrics_helper.to_h).to eq(expected_hash)
+    end
+  end
+
+  describe '#send_metrics' do
+    let(:metrics_helper) do
+      metrics_helper = described_class.new({
+                                             'Group Metadata 1': 'Group Value 1',
+                                             'Group Metadata 2': 'Group Value 2'
+                                           })
+      metrics_helper.add_metric(name: 'Metric 1', value: 12_345, meta: { m1a: 'Metric 1 Metadata A' })
+      metrics_helper.add_metric(name: 'Metric 2', value: 67_890)
+      metrics_helper.add_metric(name: 'Metric 3', value: 13_579, meta: { m3a: 'Metric 3 Metadata A', m3b: 'Metric 3 Metadata B' })
+      metrics_helper
+    end
+    let(:expected_data) do
+      {
+        meta: [
+          { name: 'Group Metadata 1', value: 'Group Value 1' },
+          { name: 'Group Metadata 2', value: 'Group Value 2' },
+        ],
+        metrics: [
+          { name: 'Metric 1', value: 12_345, meta: [{ name: 'm1a', value: 'Metric 1 Metadata A' }] },
+          { name: 'Metric 2', value: 67_890 },
+          { name: 'Metric 3', value: 13_579, meta: [{ name: 'm3a', value: 'Metric 3 Metadata A' }, { name: 'm3b', value: 'Metric 3 Metadata B' }] },
+        ]
+      }.to_json
+    end
+
+    context 'when using file:// scheme for the URL' do
+      it 'writes the payload uncompressed to a file when disabling gzip' do
+        in_tmp_dir do |tmp_dir|
+          output_file = File.join(tmp_dir, 'payload.json')
+          base_url = File.join('file://localhost/', output_file)
+
+          code = metrics_helper.send_metrics(base_url: base_url, api_token: nil, use_gzip: false)
+
+          expect(code).to eq(201)
+          expect(File).to exist(output_file)
+          uncompressed_data = File.read(output_file)
+          expect(uncompressed_data).to eq(expected_data)
+        end
+      end
+
+      it 'writes the payload compressed to a file when enabling gzip' do
+        in_tmp_dir do |tmp_dir|
+          output_file = File.join(tmp_dir, 'payload.json.gz')
+          base_url = File.join('file://localhost/', output_file)
+
+          code = metrics_helper.send_metrics(base_url: base_url, api_token: nil, use_gzip: true)
+
+          expect(code).to eq(201)
+          expect(File).to exist(output_file)
+          uncompressed_data = Zlib::GzipReader.open(output_file, &:read)
+          expect(uncompressed_data).to eq(expected_data)
+        end
+      end
+    end
+
+    context 'when using non-file:// scheme for the URL' do
+      let(:base_url) { 'https://fake-metrics-server/api/grouped-metrics' }
+      let(:token) { 'fake#tokn' }
+
+      it 'sends the payload uncompressed to the server and with the right headers when disabling gzip' do
+        expected_headers = {
+          Authorization: "Bearer #{token}",
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        }
+        last_received_body = nil
+        stub = stub_request(:post, base_url).with(headers: expected_headers) do |req|
+          last_received_body = req.body
+        end.to_return(status: 201)
+
+        code = metrics_helper.send_metrics(base_url: base_url, api_token: token, use_gzip: false)
+
+        expect(code).to eq(201)
+        expect(stub).to have_been_made.once
+        expect(last_received_body).to eq(expected_data)
+      end
+
+      it 'sends the payload compressed to the server and with the right headers when enabling gzip' do
+        expected_headers = {
+          Authorization: "Bearer #{token}",
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          'Content-Encoding': 'gzip'
+        }
+        last_received_body = nil
+        stub = stub_request(:post, base_url).with(headers: expected_headers) do |req|
+          last_received_body = req.body
+        end.to_return(status: 201)
+
+        code = metrics_helper.send_metrics(base_url: base_url, api_token: token, use_gzip: true)
+
+        expect(code).to eq(201)
+        expect(stub).to have_been_made.once
+        expect do
+          last_received_body = Zlib.gunzip(last_received_body)
+        end.not_to raise_error
+        expect(last_received_body).to eq(expected_data)
+      end
+    end
+  end
+end
