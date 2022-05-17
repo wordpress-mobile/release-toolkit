9dc580ed2f92fff4cb1de9f76eab74b295f00ab7
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2022-05-17 14:58:58 -0600

    Add docs

--- lib/fastlane/plugin/wpmreleasetoolkit/models/firebase_test_runner.rb
+++ lib/fastlane/plugin/wpmreleasetoolkit/models/firebase_test_runner.rb
@@ -27,6 +27,13 @@ module Fastlane
       @has_authenticated = true
     end
 
+    # Run a given APK and Test Bundle on the given device type.
+    #
+    # @param [String] apk_path Path to the application APK on disk.
+    # @param [String] test_apk_path Path to the test runner APK on disk.
+    # @param [FirebaseDevice] device The virtual device to run tests on.
+    # @param [String] type The type of test to run.
+    #
     def run_tests(apk_path:, test_apk_path:, device:, type: 'instrumentation')
       raise "Unable to find apk: #{apk_path}" unless File.file? apk_path
       raise "Unable to find apk: #{test_apk_path}" unless File.file? test_apk_path
@@ -55,6 +62,13 @@ module Fastlane
       result
     end
 
+    # Downloads all files associated with a Firebase Test Run to the local machine.
+    #
+    # @param [FirebaseTestLabResult] result The result bundle for a given test run.
+    # @param [String] destination The local directory to store all downloaded files.
+    # @param [String] project_id The Google Cloud Project ID – required for Google Cloud Storage access.
+    # @param [String] key_file_path The path to the key file – required for Google Cloud Storage access.
+    #
     def download_result_files(result:, destination:, project_id:, key_file_path:)
       raise unless result.is_a? Fastlane::FirebaseTestLabResult
 
@@ -77,6 +91,11 @@ module Fastlane
       files_to_download.each { |file| download_file(file: file, destination: destination) }
     end
 
+    # Download a Google Cloud Storage file to the local machine, creating intermediate directories as needed.
+    #
+    # @param [Google::Cloud::Storage::File] file Usually provided via `bucket.files`.
+    # @param [String] destination The local directory to store the file. It will retain its original name.
+    #
     def download_file(file:, destination:)
       destination = File.join(destination, file.name)
       FileUtils.mkdir_p(File.dirname(destination))
