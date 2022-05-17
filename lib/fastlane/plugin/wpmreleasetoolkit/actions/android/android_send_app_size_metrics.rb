323e622d242ff612cf40331bebd9ed2925d1514f
Olivier Halligon <olivier.halligon@automattic.com>
2022-05-17 17:51:53 +0200

    android_send_app_size_metrics action + specs

---
+++ lib/fastlane/plugin/wpmreleasetoolkit/actions/android/android_send_app_size_metrics.rb
@@ -0,0 +1,193 @@
+require_relative '../../helper/app_size_metrics_helper'
+
+module Fastlane
+  module Actions
+    class AndroidSendAppSizeMetricsAction < Action
+      def self.run(params)
+        # Check input parameters
+        base_url = URI(params[:api_base_url])
+        api_token = params[:api_token]
+        if (api_token.nil? || api_token.empty?) && !base_url.is_a?(URI::File)
+          UI.user_error!('An API token is required when using an `api_base_url` with a scheme other than `file://`')
+        end
+
+        # Build the payload base
+        metrics_helper = Fastlane::WPMRT::AppSizeMetricsHelper.new(
+          Platform: 'Android',
+          'App Name': params[:app_name],
+          'App Version': params[:app_version_name],
+          'Version Code': params[:app_version_code],
+          'Product Flavor': params[:product_flavor],
+          'Build Type': params[:build_type],
+          Source: params[:source]
+        )
+        metrics_helper.add_metric(name: 'AAB File Size', value: File.size(params[:aab_path]))
+
+        # Add device-specific 'splits' metrics to the payload if a `:include_split_sizes` is enabled
+        if params[:include_split_sizes]
+          check_bundletool_installed!
+          apkanalyzer_bin = find_apkanalyzer_binary!
+          UI.message("[App Size Metrics] Generating the various APK splits from #{params[:aab_path]}...")
+          Dir.mktmpdir('release-toolkit-android-app-size-metrics') do |tmp_dir|
+            Action.sh('bundletool', 'build-apks', '--bundle', params[:aab_path], '--output-format', 'DIRECTORY', '--output', tmp_dir)
+            apks = Dir.glob('splits/*.apk', base: tmp_dir).map { |f| File.join(tmp_dir, f) }
+            UI.message("[App Size Metrics] Generated #{apks.length} APKs.")
+
+            apks.each do |apk|
+              UI.message("[App Size Metrics] Computing file and download size of #{File.basename(apk)}...")
+              split_name = File.basename(apk, '.apk').delete_prefix('base-')
+              file_size = Action.sh(apkanalyzer_bin, 'apk', 'file-size', apk, print_command: false, print_command_output: false).chomp.to_i
+              download_size = Action.sh(apkanalyzer_bin, 'apk', 'download-size', apk, print_command: false, print_command_output: false).chomp.to_i
+              metrics_helper.add_metric(name: 'APK File Size', value: file_size, meta: { split: split_name })
+              metrics_helper.add_metric(name: 'Download Size', value: download_size, meta: { split: split_name })
+            end
+
+            UI.message('[App Size Metrics] Done computing splits sizes.')
+          end
+        end
+
+        # Send the payload
+        metrics_helper.send_metrics(
+          base_url: base_url,
+          api_token: api_token,
+          use_gzip: params[:use_gzip_content_encoding]
+        )
+      end
+
+      def self.check_bundletool_installed!
+        Action.sh('command', '-v', 'bundletool', print_command: false, print_command_output: false)
+      rescue StandardError
+        UI.user_error!('bundletool is required to build the split APKs. Install it with `brew install bundletool`')
+        raise
+      end
+
+      def self.find_apkanalyzer_binary!
+        sdk_root = ENV['ANDROID_SDK_ROOT'] || ENV['ANDROID_HOME']
+        apkanalyzer_bin = sdk_root.nil? ? Action.sh('command', '-v', 'apkanalyzer') : File.join(sdk_root, 'cmdline-tools', 'latest', 'bin', 'apkanalyzer')
+        UI.user_error!('Unable to find apkanalyzer executable. Make sure you installed the Android SDK Command-line Tools') unless File.executable?(apkanalyzer_bin)
+        apkanalyzer_bin
+      end
+
+      #####################################################
+      # @!group Documentation
+      #####################################################
+
+      def self.description
+        'Send Android app size metrics to our metrics server'
+      end
+
+      def self.details
+        <<~DETAILS
+          Send Android app size metrics to our metrics server.
+
+          See https://github.com/Automattic/apps-metrics for the API contract expected by the Metrics server you will send those metrics to.
+
+          Tip: If you provide a `file://` URL for the `api_base_url`, the action will write the payload on disk at the specified path instead of sending
+          the data to a endpoint over network. This can be useful e.g. to inspect the payload and debug it, or to store the metrics data as CI artefacts.
+        DETAILS
+      end
+
+      def self.available_options
+        [
+          FastlaneCore::ConfigItem.new(
+            key: :api_base_url,
+            env_name: 'FL_ANDROID_SEND_APP_SIZE_METRICS_API_BASE_URL',
+            description: 'The endpoint API URL to publish metrics to. (Note: you can also point to a `file://` URL to write the payload to a file instead)',
+            type: String,
+            optional: false
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :api_token,
+            env_name: 'FL_ANDROID_SEND_APP_SIZE_METRICS_API_TOKEN',
+            description: 'The bearer token to call the API. Required, unless `api_base_url` is a `file://` URL',
+            type: String,
+            optional: true
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :use_gzip_content_encoding,
+            env_name: 'FL_ANDROID_SEND_APP_SIZE_METRICS_USE_GZIP_CONTENT_ENCODING',
+            description: 'Specify that we should use `Content-Encoding: gzip` and gzip the body when sending the request',
+            type: FastlaneCore::Boolean,
+            default_value: true
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :app_name,
+            env_name: 'FL_ANDROID_SEND_APP_SIZE_METRICS_APP_NAME',
+            description: 'The name of the app for which we are publishing metrics, to help filter by app in the dashboard',
+            type: String,
+            optional: false
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :app_version_name,
+            env_name: 'FL_ANDROID_SEND_APP_SIZE_METRICS_APP_VERSION_NAME',
+            description: 'The version name of the app for which we are publishing metrics, to help filter by version in the dashboard',
+            type: String,
+            optional: false
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :app_version_code,
+            env_name: 'FL_ANDROID_SEND_APP_SIZE_METRICS_APP_VERSION_CODE',
+            description: 'The version code of the app for which we are publishing metrics, to help filter by version in the dashboard',
+            type: Integer,
+            optional: true
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :product_flavor,
+            env_name: 'FL_ANDROID_SEND_APP_SIZE_METRICS_PRODUCT_FLAVOR',
+            description: 'The product flavor for which we are publishing metrics, to help filter by flavor in the dashboard. E.g. `Vanilla`, `Jalapeno`, `Wasabi`',
+            type: String,
+            optional: true
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :build_type,
+            env_name: 'FL_ANDROID_SEND_APP_SIZE_METRICS_BUILD_TYPE',
+            description: 'The build type for which we are publishing metrics, to help filter by build type in the dashboard. E.g. `Debug`, `Release`',
+            type: String,
+            optional: true
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :source,
+            env_name: 'FL_ANDROID_SEND_APP_SIZE_METRICS_SOURCE',
+            description: 'The type of event at the origin of that build, to help filter data in the dashboard. E.g. `pr`, `beta`, `final-release`',
+            type: String,
+            optional: true
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :aab_path,
+            env_name: 'FL_ANDROID_SEND_APP_SIZE_METRICS_AAB_PATH',
+            description: 'The path to the .aab to extract size information from',
+            type: String,
+            optional: false,
+            verify_block: proc do |value|
+              UI.user_error!('You must provide an path to an existing `.aab` file') unless File.exist?(value)
+            end
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :include_split_sizes,
+            env_name: 'FL_ANDROID_SEND_APP_SIZE_METRICS_INCLUDE_SPLIT_SIZES',
+            description: 'Indicate if we should use `bundletool` and `apkanalyzer` to also compute and send "split apk" sizes per architecture. ' \
+              + 'Setting this to `true` adds a bit of extra time to generate the `.apk` and extract the data, but provides more detailed metrics',
+            type: FastlaneCore::Boolean,
+            default_value: true
+          ),
+        ]
+      end
+
+      def self.return_type
+        :integer
+      end
+
+      def self.return_value
+        'The HTTP return code from the call. Expect a 201 when new metrics were received successfully and entries created in the database'
+      end
+
+      def self.authors
+        ['automattic']
+      end
+
+      def self.is_supported?(platform)
+        platform == :android
+      end
+    end
+  end
+end
---
+++ spec/android_send_app_size_metrics_spec.rb
@@ -0,0 +1,137 @@
+require_relative './spec_helper'
+
+describe Fastlane::Actions::AndroidSendAppSizeMetricsAction do
+  let(:test_data_dir) { File.join(File.dirname(__FILE__), 'test-data', 'app_size_metrics') }
+
+  def test_app_size_action(fake_aab_size:, fake_apks:, expected_payload:, **other_action_args)
+    in_tmp_dir do |tmp_dir|
+      # Arrange
+      output_file = File.join(tmp_dir, 'output-payload')
+      aab_path = File.join(tmp_dir, 'fake.aab')
+      File.write(aab_path, '-fake-aab-file-')
+      allow(File).to receive(:size).with(aab_path).and_return(fake_aab_size)
+
+      if other_action_args[:include_split_sizes] != false
+        # Arrange: fake that apkanalyzer exists
+        ENV['ANDROID_SDK_ROOT'] = '__ANDROID_SDK_ROOT__FOR_TESTS__'
+        apkanalyzer_bin = File.join('__ANDROID_SDK_ROOT__FOR_TESTS__', 'cmdline-tools', 'latest', 'bin', 'apkanalyzer')
+        allow(File).to receive(:executable?).with(apkanalyzer_bin).and_return(true)
+
+        # Arrange: fake that bundletool exists and mock its call to create fake apks with corresponding apkanalyzer calls mocks
+        allow(Fastlane::Action).to receive(:sh).with('command', '-v', 'bundletool', anything)
+        allow(Fastlane::Action).to receive(:sh).with('bundletool', 'build-apks', '--bundle', aab_path, '--output-format', 'DIRECTORY', '--output', anything) do |*args|
+          bundletool_tmpdir = args.last
+          FileUtils.mkdir(File.join(bundletool_tmpdir, 'splits'))
+          fake_apks.each do |apk_name, sizes|
+            apk_path = File.join(bundletool_tmpdir, 'splits', apk_name.to_s)
+            File.write(apk_path, "Fake APK file (#{sizes})")
+            allow(Fastlane::Action).to receive(:sh).with(apkanalyzer_bin, 'apk', 'file-size', apk_path, anything).and_return(sizes[0].to_s)
+            allow(Fastlane::Action).to receive(:sh).with(apkanalyzer_bin, 'apk', 'download-size', apk_path, anything).and_return(sizes[1].to_s)
+          end
+        end
+      end
+
+      # Act
+      code = run_described_fastlane_action(
+        api_base_url: File.join('file://localhost/', output_file),
+        aab_path: aab_path,
+        **other_action_args
+      )
+
+      # Asserts
+      expect(code).to eq(201)
+      expect(File).to exist(output_file)
+      gzip_disabled = other_action_args[:use_gzip_content_encoding] == false
+      generated_payload = gzip_disabled ? File.read(output_file) : Zlib::GzipReader.open(output_file, &:read)
+      # Compare the payloads as pretty-formatted JSON, to make the diff in test failures more readable if one happen
+      expect(JSON.pretty_generate(JSON.parse(generated_payload))).to eq(JSON.pretty_generate(expected_payload)), 'Decompressed JSON payload was not as expected'
+      # Compare the payloads as raw uncompressed data as a final check
+      expect(generated_payload).to eq(expected_payload.to_json)
+    end
+  end
+
+  context 'when `include_split_sizes` is turned off' do
+    it 'generates the expected payload compressed by default' do
+      expected = {
+        meta: [
+          { name: 'Platform', value: 'Android' },
+          { name: 'App Name', value: 'my-app' },
+          { name: 'App Version', value: '10.2-rc-3' },
+          { name: 'Product Flavor', value: 'Vanilla' },
+          { name: 'Build Type', value: 'Release' },
+          { name: 'Source', value: 'unit-test' },
+        ],
+        metrics: [
+          { name: 'AAB File Size', value: 123_456 },
+        ]
+      }
+
+      test_app_size_action(
+        fake_aab_size: 123_456,
+        fake_apks: {},
+        expected_payload: expected,
+        app_name: 'my-app',
+        app_version_name: '10.2-rc-3',
+        product_flavor: 'Vanilla',
+        build_type: 'Release',
+        source: 'unit-test',
+        include_split_sizes: false
+      )
+    end
+
+    it 'generates the expected payload uncompressed when disabling gzip' do
+      expected = {
+        meta: [
+          { name: 'Platform', value: 'Android' },
+          { name: 'App Name', value: 'my-app' },
+          { name: 'App Version', value: '10.2-rc-3' },
+          { name: 'Product Flavor', value: 'Vanilla' },
+          { name: 'Build Type', value: 'Release' },
+          { name: 'Source', value: 'unit-test' },
+        ],
+        metrics: [
+          { name: 'AAB File Size', value: 123_456 },
+        ]
+      }
+
+      test_app_size_action(
+        fake_aab_size: 123_456,
+        fake_apks: {},
+        expected_payload: expected,
+        app_name: 'my-app',
+        app_version_name: '10.2-rc-3',
+        product_flavor: 'Vanilla',
+        build_type: 'Release',
+        source: 'unit-test',
+        include_split_sizes: false,
+        use_gzip_content_encoding: false
+      )
+    end
+  end
+
+  context 'when keeping the default value of `include_split_sizes` turned on' do
+    it 'generates the expected payload containing the aab file size and optimized split sizes' do
+      expected_fixture = File.join(test_data_dir, 'android-metrics-payload.json')
+      expected = JSON.parse(File.read(expected_fixture))
+
+      test_app_size_action(
+        fake_aab_size: 987_654_321,
+        fake_apks: {
+          'base-arm64_v8a.apk': [164_080, 64_080],
+          'base-arm64_v8a_2.apk': [164_082, 64_082],
+          'base-armeabi.apk': [150_000, 50_000],
+          'base-armeabi_2.apk': [150_002, 50_002],
+          'base-armeabi_v7a.apk': [150_070, 50_070],
+          'base-armeabi_v7a_2.apk': [150_072, 50_072]
+        },
+        expected_payload: expected,
+        app_name: 'wordpress',
+        app_version_name: '19.8-rc-3',
+        app_version_code: 1214,
+        product_flavor: 'Vanilla',
+        build_type: 'Release',
+        source: 'unit-test'
+      )
+    end
+  end
+end
