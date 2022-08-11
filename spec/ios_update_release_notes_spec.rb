0155527605e21917db5a60d03e30b4c54d8d4163
Leandro Alonso <contato@leandroalonso.com>
2022-08-11 14:00:05 -0300

    Updates ios_update_release_notes to accepts an argument with the changelog file path

--- spec/ios_update_release_notes_spec.rb
+++ spec/ios_update_release_notes_spec.rb
@@ -2,15 +2,15 @@ require 'spec_helper'
 
 describe Fastlane::Actions::IosUpdateReleaseNotesAction do
   let(:release_notes_txt) { File.join(File.dirname(__FILE__), 'RELEASE-NOTES.txt') }
+  let(:changelog_md) { File.join(File.dirname(__FILE__), 'CHANGELOG.md') }
 
   after do
-    FileUtils.remove_entry release_notes_txt
+    FileUtils.rm([release_notes_txt, changelog_md], force: true)
   end
 
   describe '#ios_update_release_notes' do
     it 'adds a new section on RELEASE-NOTES.txt' do
       # Arrange
-
       ENV['PROJECT_ROOT_FOLDER'] = File.dirname(__FILE__)
       File.write(release_notes_txt, '')
 
@@ -21,6 +21,21 @@ describe Fastlane::Actions::IosUpdateReleaseNotesAction do
 
       # Assert
       expect(File.read(release_notes_txt)).to eq("1.1\n-----\n\n\n")
+    end
+
+    it 'adds a new section on the given file' do
+      # Arrange
+      ENV['PROJECT_ROOT_FOLDER'] = File.dirname(__FILE__)
+      File.write(changelog_md, '')
+
+      # Act
+      run_described_fastlane_action(
+        new_version: '1.0',
+        release_notes_file_path: changelog_md
+      )
+
+      # Assert
+      expect(File.read(changelog_md)).to eq("1.1\n-----\n\n\n")
     end
   end
 end
