9d4b18a2ff1b408150c79588a89dd315392a6956
Leandro Alonso <contato@leandroalonso.com>
2022-08-11 14:36:21 -0300

    Refactor ios_update_release_notes to avoid creating files within the repo

--- spec/ios_update_release_notes_spec.rb
+++ spec/ios_update_release_notes_spec.rb
@@ -1,41 +1,40 @@
 require 'spec_helper'
 
 describe Fastlane::Actions::IosUpdateReleaseNotesAction do
-  let(:release_notes_txt) { File.join(File.dirname(__FILE__), 'RELEASE-NOTES.txt') }
-  let(:changelog_md) { File.join(File.dirname(__FILE__), 'CHANGELOG.md') }
-
-  after do
-    FileUtils.rm([release_notes_txt, changelog_md], force: true)
-  end
-
   describe '#ios_update_release_notes' do
     it 'adds a new section on RELEASE-NOTES.txt' do
-      # Arrange
-      ENV['PROJECT_ROOT_FOLDER'] = File.dirname(__FILE__)
-      File.write(release_notes_txt, '')
+      in_tmp_dir do |tmp_dir|
+        # Arrange
+        ENV['PROJECT_ROOT_FOLDER'] = tmp_dir
+        release_notes_txt = File.join(tmp_dir, 'RELEASE-NOTES.txt')
+        File.write(release_notes_txt, '')
 
-      # Act
-      run_described_fastlane_action(
-        new_version: '1.0'
-      )
+        # Act
+        run_described_fastlane_action(
+          new_version: '1.0'
+        )
 
-      # Assert
-      expect(File.read(release_notes_txt)).to eq("1.1\n-----\n\n\n")
+        # Assert
+        expect(File.read(release_notes_txt)).to eq("1.1\n-----\n\n\n")
+      end
     end
 
     it 'adds a new section on the given file' do
-      # Arrange
-      ENV['PROJECT_ROOT_FOLDER'] = File.dirname(__FILE__)
-      File.write(changelog_md, '')
+      in_tmp_dir do |tmp_dir|
+        # Arrange
+        ENV['PROJECT_ROOT_FOLDER'] = tmp_dir
+        changelog_md = File.join(tmp_dir, 'CHANGELOG.md')
+        File.write(changelog_md, '')
 
-      # Act
-      run_described_fastlane_action(
-        new_version: '1.0',
-        release_notes_file_path: changelog_md
-      )
+        # Act
+        run_described_fastlane_action(
+          new_version: '1.0',
+          release_notes_file_path: changelog_md
+        )
 
-      # Assert
-      expect(File.read(changelog_md)).to eq("1.1\n-----\n\n\n")
+        # Assert
+        expect(File.read(changelog_md)).to eq("1.1\n-----\n\n\n")
+      end
     end
   end
 end
