5d880122cc125dee09d32a8fa6a3d6134da31894
Leandro Alonso <contato@leandroalonso.com>
2022-08-11 14:04:05 -0300

    Adds a optional release_notes_file_path arg to android_update_release_notes

--- lib/fastlane/plugin/wpmreleasetoolkit/actions/android/android_update_release_notes.rb
+++ lib/fastlane/plugin/wpmreleasetoolkit/actions/android/android_update_release_notes.rb
@@ -8,7 +8,7 @@ module Fastlane
         require_relative '../../helper/release_notes_helper'
         require_relative '../../helper/git_helper'
 
-        path = File.join(ENV['PROJECT_ROOT_FOLDER'] || '.', 'RELEASE-NOTES.txt')
+        path = params[:release_notes_file_path]
         next_version = Fastlane::Helper::Android::VersionHelper.calc_next_release_short_version(params[:new_version])
 
         Fastlane::Helper::ReleaseNotesHelper.add_new_section(path: path, section_title: next_version)
@@ -35,6 +35,11 @@ module Fastlane
                                        env_name: 'FL_ANDROID_UPDATE_RELEASE_NOTES_VERSION',
                                        description: 'The version we are currently freezing; An empty entry for the _next_ version after this one will be added to the release notes',
                                        is_string: true),
+          FastlaneCore::ConfigItem.new(key: :release_notes_file_path,
+                                       env_name: 'FL_ANDROID_UPDATE_RELEASE_NOTES_FILE_PATH',
+                                       description: 'The path to the release notes file to be updated',
+                                       is_string: true,
+                                       default_value: File.join(ENV['PROJECT_ROOT_FOLDER'] || '.', 'RELEASE-NOTES.txt')),
         ]
       end
 
--- spec/android_update_release_notes_spec.rb
+++ spec/android_update_release_notes_spec.rb
@@ -2,9 +2,10 @@ require 'spec_helper'
 
 describe Fastlane::Actions::AndroidUpdateReleaseNotesAction do
   let(:release_notes_txt) { File.join(File.dirname(__FILE__), 'RELEASE-NOTES.txt') }
+  let(:changelog_md) { File.join(File.dirname(__FILE__), 'CHANGELOG.md') }
 
   after do
-    FileUtils.remove_entry release_notes_txt
+    FileUtils.rm([release_notes_txt, changelog_md], force: true)
   end
 
   describe '#android_update_release_notes' do
@@ -21,6 +22,21 @@ describe Fastlane::Actions::AndroidUpdateReleaseNotesAction do
 
       # Assert
       expect(File.read(release_notes_txt)).to eq("1.1\n-----\n\n\n")
+    end
+
+    it 'adds a new section on the given file' do
+      # Arrange
+      ENV['PROJECT_ROOT_FOLDER'] = File.dirname(__FILE__)
+      File.write(changelog_md, '')
+
+      # Act
+      run_described_fastlane_action(
+        new_version: '1.0',
+        release_notes_file_path: changelog_md
+      )
+
+      # Assert
+      expect(File.read(changelog_md)).to eq("1.1\n-----\n\n\n")
     end
   end
 end
