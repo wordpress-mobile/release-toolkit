fa93f400d7653c9909f0ffb1f2767ecbcffa9765
Leandro Alonso <contato@leandroalonso.com>
2022-08-11 13:44:18 -0300

    Add tests for android_update_release_notes

---
+++ spec/android_update_release_notes_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe Fastlane::Actions::AndroidUpdateReleaseNotesAction do
+  let(:release_notes_txt) { File.join(File.dirname(__FILE__), 'RELEASE-NOTES.txt') }
+
+  after do
+    FileUtils.remove_entry release_notes_txt
+  end
+
+  describe '#android_update_release_notes' do
+    it 'adds a new section on RELEASE-NOTES.txt' do
+      # Arrange
+
+      ENV['PROJECT_ROOT_FOLDER'] = File.dirname(__FILE__)
+      File.write(release_notes_txt, '')
+
+      # Act
+      run_described_fastlane_action(
+        new_version: '1.0'
+      )
+
+      # Assert
+      expect(File.read(release_notes_txt)).to eq("1.1\n-----\n\n\n")
+    end
+  end
+end
