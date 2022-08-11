cd42ef4abcf374721c61a716422d2b242b4cd45b
Leandro Alonso <contato@leandroalonso.com>
2022-08-11 15:15:33 -0300

    Add some content into the fixture for the iOS update release notes test

--- spec/ios_update_release_notes_spec.rb
+++ spec/ios_update_release_notes_spec.rb
@@ -7,7 +7,7 @@ describe Fastlane::Actions::IosUpdateReleaseNotesAction do
         # Arrange
         ENV['PROJECT_ROOT_FOLDER'] = tmp_dir
         release_notes_txt = File.join(tmp_dir, 'RELEASE-NOTES.txt')
-        File.write(release_notes_txt, '')
+        File.write(release_notes_txt, FAKE_CONTENT)
 
         # Act
         run_described_fastlane_action(
@@ -15,7 +15,7 @@ describe Fastlane::Actions::IosUpdateReleaseNotesAction do
         )
 
         # Assert
-        expect(File.read(release_notes_txt)).to eq("1.1\n-----\n\n\n")
+        expect(File.read(release_notes_txt)).to eq(NEW_SECTION + FAKE_CONTENT)
       end
     end
 
@@ -24,7 +24,7 @@ describe Fastlane::Actions::IosUpdateReleaseNotesAction do
         # Arrange
         ENV['PROJECT_ROOT_FOLDER'] = tmp_dir
         changelog_md = File.join(tmp_dir, 'CHANGELOG.md')
-        File.write(changelog_md, '')
+        File.write(changelog_md, FAKE_CONTENT)
 
         # Act
         run_described_fastlane_action(
@@ -33,8 +33,29 @@ describe Fastlane::Actions::IosUpdateReleaseNotesAction do
         )
 
         # Assert
-        expect(File.read(changelog_md)).to eq("1.1\n-----\n\n\n")
+        expect(File.read(changelog_md)).to eq(NEW_SECTION + FAKE_CONTENT)
       end
     end
   end
+
+  FAKE_CONTENT = <<~CONTENT
+    1.0
+    -----
+    - Item 1 for v1.0
+    - Item 2 for v1.0
+
+    // Comment in the middle
+
+    0.9.0
+    -----
+    - Item 1 for v0.9.0
+    - Item 2 for v0.9.0
+  CONTENT
+
+  NEW_SECTION = <<~CONTENT
+    1.1
+    -----
+
+
+  CONTENT
 end
