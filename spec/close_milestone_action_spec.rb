3472f9eb598ec8326a3393d5955757d3f0ddba71
Rafael Lima <rafael.lima2403@gmail.com>
2022-11-04 01:32:14 +0000

    CloseMilestoneAction: Fix Issues Pointed on PR

--- spec/close_milestone_action_spec.rb
+++ spec/close_milestone_action_spec.rb
@@ -1,105 +1,86 @@
 require 'spec_helper'
 
 describe Fastlane::Actions::CloseMilestoneAction do
-  describe 'initialize' do
-    let(:test_token) { 'Test-GithubToken-1234' }
-    let(:mock_params) do
-      {
-        repository: 'test-repository',
-        milestone: '10'
-      }
-    end
-    let(:client) do
-      instance_double(
-        Octokit::Client,
-        list_milestones: [{ title: '10.1' }],
-        update_milestone: nil,
-        user: instance_double('User', name: 'test'),
-        'auto_paginate=': nil
-      )
-    end
+  let(:test_repository) { 'test-repository' }
+  let(:test_token) { 'Test-GithubToken-1234' }
+  let(:test_milestone) do
+    { title: '10.1', number: '1234' }
+  end
+  let(:client) do
+    instance_double(
+      Octokit::Client,
+      list_milestones: [test_milestone],
+      update_milestone: nil,
+      user: instance_double('User', name: 'test'),
+      'auto_paginate=': nil
+    )
+  end
 
-    before do
+  before do
+    allow(Octokit::Client).to receive(:new).and_return(client)
+  end
+
+  it 'properly passes the environment variable `GITHUB_TOKEN` to Octokit::Client' do
+    ENV['GITHUB_TOKEN'] = test_token
+    expect(Octokit::Client).to receive(:new).with(access_token: test_token)
+    run_action_without_key(:github_token)
+  end
+
+  it 'properly passes the parameter `:github_token` to Octokit::Client' do
+    expect(Octokit::Client).to receive(:new).with(access_token: test_token)
+    run_described_fastlane_action(default_params)
+  end
+
+  it 'prioritizes `:github_token` parameter over `GITHUB_TOKEN` environment variable if both are present' do
+    ENV['GITHUB_TOKEN'] = 'Test-EnvGithubToken-1234'
+    expect(Octokit::Client).to receive(:new).with(access_token: test_token)
+    run_described_fastlane_action(default_params)
+  end
+
+  it 'properly passes the repository and milestone to Octokit::Client to update the milestone as closed' do
+    expect(client).to receive(:update_milestone).with(test_repository, test_milestone[:number], state: 'closed')
+    run_described_fastlane_action(default_params)
+  end
+
+  it 'raises an error when the milestone is not found or does not exist' do
+    allow(client).to receive(:list_milestones).and_return([])
+    expect { run_described_fastlane_action(default_params) }.to raise_error(FastlaneCore::Interface::FastlaneError, 'Milestone 10.1 not found.')
+  end
+
+  describe 'Calling the Action validates input' do
+    it 'raises an error if no `GITHUB_TOKEN` environment variable nor parameter `:github_token` is present' do
       ENV['GITHUB_TOKEN'] = nil
-      mock_params[:github_token] = nil
-      allow(Octokit::Client).to receive(:new).and_return(client)
+      expect { run_action_without_key(:github_token) }.to raise_error(FastlaneCore::Interface::FastlaneError, "No value found for 'github_token'")
     end
 
-    it 'properly passes the environment variable `GITHUB_TOKEN` all the way to Octokit::Client' do
-      ENV['GITHUB_TOKEN'] = test_token
-      expect(Octokit::Client).to receive(:new).with(access_token: test_token)
-      run_described_fastlane_action(mock_params)
+    it 'raises an error if no `GHHELPER_REPOSITORY` environment variable nor parameter `:repository` is present' do
+      expect { run_action_without_key(:repository) }.to raise_error(FastlaneCore::Interface::FastlaneError, "No value found for 'repository'")
     end
 
-    it 'properly passes the parameter `:github_token` all the way to Octokit::Client' do
-      mock_params[:github_token] = test_token
-      expect(Octokit::Client).to receive(:new).with(access_token: test_token)
-      run_described_fastlane_action(mock_params)
+    it 'raises an error if no `GHHELPER_MILESTONE` environment variable nor parameter `:milestone` is present' do
+      expect { run_action_without_key(:milestone) }.to raise_error(FastlaneCore::Interface::FastlaneError, "No value found for 'milestone'")
     end
 
-    it 'prioritizes `:github_token` parameter over `GITHUB_TOKEN` enviroment variable if both are present' do
-      ENV['GITHUB_TOKEN'] = 'Test-EnvGithubToken-1234'
-      mock_params[:github_token] = test_token
-      expect(Octokit::Client).to receive(:new).with(access_token: test_token)
-      run_described_fastlane_action(mock_params)
-    end
-
-    it 'prints an error if no `GITHUB_TOKEN` environment variable nor parameter `:github_token` is present' do
-      expect { run_described_fastlane_action(mock_params) }.to raise_error(FastlaneCore::Interface::FastlaneError)
+    it 'raises an error if `milestone:` parameter is passed as Integer' do
+      expect { run_action_with(:milestone, 10) }.to raise_error "'milestone' value must be a String! Found Integer instead."
     end
   end
 
-  describe 'get_milestone' do
-    let(:test_repository) { 'test-repository' }
-    let(:test_milestone) { '10' }
-    let(:mock_params) do
-      {
-        repository: test_repository,
-        milestone: test_milestone,
-        github_token: 'Test-GithubToken-1234'
-      }
-    end
-    let(:client) do
-      instance_double(
-        Octokit::Client,
-        list_milestones: [{ title: '10.1' }],
-        update_milestone: nil,
-        user: instance_double('User', name: 'test'),
-        'auto_paginate=': nil
-      )
-    end
-
-    it 'properly passes the repository all the way down to the Octokit::Client to list the milestones' do
-      allow(Octokit::Client).to receive(:new).and_return(client)
-      expect(client).to receive(:list_milestones).with(test_repository)
-      run_described_fastlane_action(mock_params)
-    end
+  def run_action_without_key(key)
+    run_described_fastlane_action(default_params.except(key))
   end
 
-  describe 'update_milestone' do
-    let(:test_repository) { 'test-repository' }
-    let(:test_milestone_number) { '1234' }
-    let(:mock_params) do
-      {
-        repository: test_repository,
-        milestone: '10',
-        github_token: 'Test-GithubToken-1234'
-      }
-    end
-    let(:client) do
-      instance_double(
-        Octokit::Client,
-        list_milestones: [{ title: '10.1', number: test_milestone_number }],
-        update_milestone: nil,
-        user: instance_double('User', name: 'test'),
-        'auto_paginate=': nil
-      )
-    end
+  def run_action_with(key, value)
+    values = default_params
+    values[key] = value
+    run_described_fastlane_action(values)
+  end
 
-    it 'properly passes the parameters all the way down to Octokit::Client' do
-      allow(Octokit::Client).to receive(:new).and_return(client)
-      expect(client).to receive(:update_milestone).with(test_repository, test_milestone_number, { state: 'closed' })
-      run_described_fastlane_action(mock_params)
-    end
+  def default_params
+    {
+      repository: test_repository,
+      milestone: test_milestone[:title],
+      github_token: test_token
+    }
   end
 end
