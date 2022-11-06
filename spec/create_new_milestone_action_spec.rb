9eb95c2b10b7771a31b5039f9f26dff123cea1a9
Rafael Lima <rafael.lima2403@gmail.com>
2022-11-06 15:01:42 +0000

    CreateMilestoneAction: Add Shared Examples of GithubToken

--- spec/create_new_milestone_action_spec.rb
+++ spec/create_new_milestone_action_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
+require 'shared_examples_for_actions_with_github_token'
 
 describe Fastlane::Actions::CreateNewMilestoneAction do
   let(:test_repository) { 'test-repository' }
-  let(:test_token) { 'Test-GithubToken-1234' }
   let(:test_milestone) do
     { title: '10.1', number: '1234', due_on: '2022-10-31T07:00:00Z' }
   end
@@ -12,6 +12,11 @@ describe Fastlane::Actions::CreateNewMilestoneAction do
       { title: '10.3', number: '4567', due_on: '2022-11-02T15:00:00Z' },
       { title: '10.4', number: '7890', due_on: '2022-11-04T23:59:00Z' },
     ]
+  end
+  let(:default_params) do
+    { repository: test_repository,
+      need_appstore_submission: false,
+      github_token: 'Test-GithubToken-1234' }
   end
   let(:client) do
     instance_double(
@@ -27,54 +32,41 @@ describe Fastlane::Actions::CreateNewMilestoneAction do
     allow(Octokit::Client).to receive(:new).and_return(client)
   end
 
-  it 'computes the correct due date and milestone description' do
-    comment = "Code freeze: November 14, 2022\nApp Store submission: November 28, 2022\nRelease: November 28, 2022\n"
-    expect(client).to receive(:create_milestone).with(test_repository, '10.2', due_on: '2022-11-14T12:00:00Z', description: comment)
-    run_described_fastlane_action(default_params)
-  end
+  describe 'date computation is correct' do
+    it 'computes the correct due date and milestone description' do
+      comment = "Code freeze: November 14, 2022\nApp Store submission: November 28, 2022\nRelease: November 28, 2022\n"
+      expect(client).to receive(:create_milestone).with(test_repository, '10.2', due_on: '2022-11-14T12:00:00Z', description: comment)
+      run_described_fastlane_action(default_params)
+    end
 
-  it 'removes 3 days from the AppStore submission date when `:need_appstore_submission` is true' do
-    comment = "Code freeze: November 14, 2022\nApp Store submission: November 25, 2022\nRelease: November 28, 2022\n"
-    expect(client).to receive(:create_milestone).with(test_repository, '10.2', due_on: '2022-11-14T12:00:00Z', description: comment)
-    run_action_with(:need_appstore_submission, true)
-  end
+    it 'removes 3 days from the AppStore submission date when `:need_appstore_submission` is true' do
+      comment = "Code freeze: November 14, 2022\nApp Store submission: November 25, 2022\nRelease: November 28, 2022\n"
+      expect(client).to receive(:create_milestone).with(test_repository, '10.2', due_on: '2022-11-14T12:00:00Z', description: comment)
+      run_action_with(need_appstore_submission: true)
+    end
 
-  it 'uses the most recent milestone date to calculate the due date and version of new milestone' do
-    comment = "Code freeze: November 18, 2022\nApp Store submission: December 02, 2022\nRelease: December 02, 2022\n"
-    allow(client).to receive(:list_milestones).and_return(milestone_list)
-    expect(client).to receive(:create_milestone).with(test_repository, '10.5', due_on: '2022-11-18T12:00:00Z', description: comment)
-    run_described_fastlane_action(default_params)
-  end
+    it 'uses the most recent milestone date to calculate the due date and version of new milestone' do
+      comment = "Code freeze: November 18, 2022\nApp Store submission: December 02, 2022\nRelease: December 02, 2022\n"
+      allow(client).to receive(:list_milestones).and_return(milestone_list)
+      expect(client).to receive(:create_milestone).with(test_repository, '10.5', due_on: '2022-11-18T12:00:00Z', description: comment)
+      run_described_fastlane_action(default_params)
+    end
 
-  it 'raises an error when the due date of milestone does not exists' do
-    allow(client).to receive(:list_milestones).and_return([{ title: '10.1', number: '1234' }])
-    expect { run_described_fastlane_action(default_params) }.to raise_error(FastlaneCore::Interface::FastlaneError, 'Milestone 10.1 has no due date.')
-  end
+    context 'when last milestone cannot be used' do
+      it 'raises an error when the due date of milestone does not exists' do
+        allow(client).to receive(:list_milestones).and_return([{ title: '10.1', number: '1234' }])
+        expect { run_described_fastlane_action(default_params) }.to raise_error(FastlaneCore::Interface::FastlaneError, 'Milestone 10.1 has no due date.')
+      end
 
-  it 'raises an error when the milestone is not found or does not exist on the repository' do
-    allow(client).to receive(:list_milestones).and_return([])
-    expect { run_described_fastlane_action(default_params) }.to raise_error(FastlaneCore::Interface::FastlaneError, 'No milestone found on the repository.')
+      it 'raises an error when the milestone is not found or does not exist on the repository' do
+        allow(client).to receive(:list_milestones).and_return([])
+        expect { run_described_fastlane_action(default_params) }.to raise_error(FastlaneCore::Interface::FastlaneError, 'No milestone found on the repository.')
+      end
+    end
   end
 
   describe 'initialize' do
-    context 'with github_token' do
-      it 'properly passes the environment variable `GITHUB_TOKEN` to Octokit::Client' do
-        ENV['GITHUB_TOKEN'] = test_token
-        expect(Octokit::Client).to receive(:new).with(access_token: test_token)
-        run_action_without_key(:github_token)
-      end
-
-      it 'properly passes the parameter `:github_token` to Octokit::Client' do
-        expect(Octokit::Client).to receive(:new).with(access_token: test_token)
-        run_described_fastlane_action(default_params)
-      end
-
-      it 'prioritizes `:github_token` parameter over `GITHUB_TOKEN` environment variable if both are present' do
-        ENV['GITHUB_TOKEN'] = 'Test-EnvGithubToken-1234'
-        expect(Octokit::Client).to receive(:new).with(access_token: test_token)
-        run_described_fastlane_action(default_params)
-      end
-    end
+    include_examples 'github_token_initialization'
 
     context 'when using default parameters' do
       let(:github_helper) do
@@ -118,43 +110,29 @@ describe Fastlane::Actions::CreateNewMilestoneAction do
   end
 
   describe 'calling the action validates input' do
-    it 'raises an error if no `GITHUB_TOKEN` environment variable nor parameter `:github_token` is present' do
-      ENV['GITHUB_TOKEN'] = nil
-      expect { run_action_without_key(:github_token) }.to raise_error(FastlaneCore::Interface::FastlaneError, "No value found for 'github_token'")
-    end
-
     it 'raises an error if no `GHHELPER_REPOSITORY` environment variable nor parameter `:repository` is present' do
-      expect { run_action_without_key(:repository) }.to raise_error(FastlaneCore::Interface::FastlaneError, "No value found for 'repository'")
+      expect { run_action_without(:repository) }.to raise_error(FastlaneCore::Interface::FastlaneError, "No value found for 'repository'")
     end
 
     it 'raises an error if `need_appstore_submission:` parameter is passed as String' do
-      expect { run_action_with(:need_appstore_submission, 'foo') }.to raise_error "'need_appstore_submission' value must be either `true` or `false`! Found String instead."
+      expect { run_action_with(need_appstore_submission: 'foo') }.to raise_error "'need_appstore_submission' value must be either `true` or `false`! Found String instead."
     end
 
     it 'raises an error if `milestone_duration:` parameter is passed as String' do
-      expect { run_action_with(:milestone_duration, 'foo') }.to raise_error "'milestone_duration' value must be a Integer! Found String instead."
+      expect { run_action_with(milestone_duration: 'foo') }.to raise_error "'milestone_duration' value must be a Integer! Found String instead."
     end
 
     it 'raises an error if `number_of_days_from_code_freeze_to_release:` parameter is passed as String' do
-      expect { run_action_with(:number_of_days_from_code_freeze_to_release, 'foo') }.to raise_error "'number_of_days_from_code_freeze_to_release' value must be a Integer! Found String instead."
+      expect { run_action_with(number_of_days_from_code_freeze_to_release: 'foo') }.to raise_error "'number_of_days_from_code_freeze_to_release' value must be a Integer! Found String instead."
     end
   end
 
-  def run_action_without_key(key)
+  def run_action_with(**keys_and_values)
+    params = default_params.merge(keys_and_values)
+    run_described_fastlane_action(params)
+  end
+
+  def run_action_without(key)
     run_described_fastlane_action(default_params.except(key))
-  end
-
-  def run_action_with(key, value)
-    values = default_params
-    values[key] = value
-    run_described_fastlane_action(values)
-  end
-
-  def default_params
-    {
-      repository: test_repository,
-      need_appstore_submission: false,
-      github_token: test_token
-    }
   end
 end
