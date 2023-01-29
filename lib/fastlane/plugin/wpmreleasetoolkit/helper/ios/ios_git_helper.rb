000862d2cef2e551ce13f3dd2aad3f3d1d415808
Ian Guedes Maia <ian.guedesmaia@getbux.com>
2023-01-29 21:04:17 +0100

    Remove Deliverfile related functionality and references

--- lib/fastlane/plugin/wpmreleasetoolkit/helper/ios/ios_git_helper.rb
+++ lib/fastlane/plugin/wpmreleasetoolkit/helper/ios/ios_git_helper.rb
@@ -8,15 +8,11 @@ module Fastlane
         #
         # This typically commits:
         #  - The files in `./config/*` – especially `Version.*.xcconfig` files
-        #  - The `fastlane/Deliverfile` file (which contains the `app_version` line)
         #
         # @env PROJECT_ROOT_FOLDER The path to the git root of the project
         #
-        # @param [Bool] include_deliverfile If true (the default), includes the `fastlane/Deliverfile` in files being commited
-        #
-        def self.commit_version_bump(include_deliverfile: true)
+        def self.commit_version_bump
           files_list = [File.join(get_from_env!(key: 'PROJECT_ROOT_FOLDER'), 'config', '.')]
-          files_list.append File.join('fastlane', 'Deliverfile') if include_deliverfile
 
           Fastlane::Helper::GitHelper.commit(message: 'Bump version number', files: files_list)
         end
