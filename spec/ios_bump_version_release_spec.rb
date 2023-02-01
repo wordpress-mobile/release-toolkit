60a350cb4b5abf5ab331829dba650366e4e614ad
Ian Guedes Maia <ian.guedesmaia@getbux.com>
2023-02-01 16:40:05 +0100

    Fix unit test that broke due changes in `Ios::VersionHelper`

--- spec/ios_bump_version_release_spec.rb
+++ spec/ios_bump_version_release_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Fastlane::Actions::IosBumpVersionReleaseAction do
   let(:default_branch) { 'my_new_branch' }
-  let(:versions) { ['6.30'] }
+  let(:version) { '6.30' }
   let(:next_version) { '6.31.0.0' }
   let(:next_version_short) { '6.31' }
 
@@ -15,7 +15,7 @@ describe Fastlane::Actions::IosBumpVersionReleaseAction do
       allow(Fastlane::Helper::GitHelper).to receive(:checkout_and_pull).with(default_branch)
       allow(Fastlane::Helper::GitHelper).to receive(:create_branch).with("release/#{next_version_short}", from: default_branch)
 
-      allow(Fastlane::Helper::Ios::VersionHelper).to receive(:get_version_strings).and_return(versions)
+      allow(Fastlane::Helper::Ios::VersionHelper).to receive(:read_from_config_file).and_return(version)
       allow(Fastlane::Helper::Ios::VersionHelper).to receive(:update_xc_configs).with(next_version, next_version_short, nil)
     end
 
