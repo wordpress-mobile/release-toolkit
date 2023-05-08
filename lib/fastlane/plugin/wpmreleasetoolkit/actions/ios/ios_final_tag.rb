e5cf5d8bbe97f02755e48a4d019c75a0a34eacff
Oguz Kocer <oguz.kocer@automattic.com>
2023-05-08 19:01:21 -0400

    Deprecate ios_clear_intermediate_tags & ios_final_tag actions

--- lib/fastlane/plugin/wpmreleasetoolkit/actions/ios/ios_final_tag.rb
+++ lib/fastlane/plugin/wpmreleasetoolkit/actions/ios/ios_final_tag.rb
@@ -2,6 +2,8 @@ module Fastlane
   module Actions
     class IosFinalTagAction < Action
       def self.run(params)
+        return unless UI.confirm("#{deprecated_notes} Would you like to continue with the action?")
+
         require_relative '../../helper/ios/ios_git_helper'
         require_relative '../../helper/ios/ios_version_helper'
         version = Fastlane::Helper::Ios::VersionHelper.get_public_version
@@ -38,6 +40,12 @@ module Fastlane
       end
 
       def self.return_value
+      end
+
+      def self.deprecated_notes
+        "This action is deprecated as we don't believe it's currently in use in our projects.
+        However, just to be sure that it's not in use, we decided to deprecate it first. If you
+        believe that this is a mistake, please let us know on Slack."
       end
 
       def self.authors
