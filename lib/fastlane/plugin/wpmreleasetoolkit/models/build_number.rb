a6ba0ae1388f1b5d196e47e7652a5ce8aefaa19d
Spencer Transier <spencer@transier.us>
2023-07-19 12:59:31 -0700

    Update bump_build_number

--- lib/fastlane/plugin/wpmreleasetoolkit/models/build_number.rb
+++ lib/fastlane/plugin/wpmreleasetoolkit/models/build_number.rb
@@ -3,14 +3,14 @@ require 'xcodeproj'
 module Fastlane
   module Model
     class BuildNumber
-      attr_accessor :file_path, :build_number
+      attr_accessor :file_path
 
       def initialize(file_path)
         @file_path = file_path
       end
 
       def get_build_number
-        read_build_number_from_xcconfig
+        read_build_number_from_xcconfig.to_s
       end
 
       def read_build_number_from_xcconfig
@@ -21,17 +21,17 @@ module Fastlane
         config.attributes['BUILD_NUMBER']
       end
 
-      def write_build_number_to_xcconfig
+      def write_build_number_to_xcconfig(build_number)
         if File.exist?(@file_path)
-          new_build_number = bump_build_number(@build_number)
-          Action.sh("sed -i '' \"$(awk '/^BUILD_NUMBER/{ print NR; exit }' \"#{@file_path}\")s/=.*/=#{new_build_number}/\" \"#{@file_path}\"")
+          Action.sh("sed -i '' \"$(awk '/^BUILD_NUMBER/{ print NR; exit }' \"#{@file_path}\")s/=.*/=#{build_number}/\" \"#{@file_path}\"")
         else
           UI.user_error!("#{@file_path} not found")
         end
       end
 
-      def bump_build_number(build_number)
-        build_number.nil? ? 0 : build_number.to_i + 1
+      def bump_build_number
+        new_build_number = get_build_number.nil? ? 0 : build_number.to_i + 1
+        write_build_number_to_xcconfig(new_build_number)
       end
     end
   end
