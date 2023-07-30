21ba03bc7842f6d28920ffa5dd9facd73f87acdf
Spencer Transier <spencer@transier.us>
2023-07-29 20:10:54 -0700

    Add `Version` model

---
+++ lib/fastlane/plugin/wpmreleasetoolkit/models/version.rb
@@ -0,0 +1,10 @@
+class Version
+  attr_accessor :major, :minor, :patch, :build_number
+
+  def initialize(major, minor, patch, build_number)
+    @major = major
+    @minor = minor
+    @patch = patch
+    @build_count = build_count
+  end
+end
