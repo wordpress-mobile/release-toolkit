276cc3e3d99b5a53230098e028d1e94da6b40826
Gio Lodi <gio@mokacoding.com>
2023-08-31 09:17:16 +1000

    Extract `UnknownMetadataBlock` in dedicated file

--- lib/fastlane/plugin/wpmreleasetoolkit/helper/metadata_update_helper.rb
+++ lib/fastlane/plugin/wpmreleasetoolkit/helper/metadata_update_helper.rb
@@ -2,14 +2,6 @@ require_relative 'metadata_block'
 
 module Fastlane
   module Helper
-    class UnknownMetadataBlock < MetadataBlock
-      attr_reader :content_file_path
-
-      def initialize
-        super(nil)
-      end
-    end
-
     class StandardMetadataBlock < MetadataBlock
       attr_reader :content_file_path
 
