ccc2b67f15ddc9413465b76d627ee326f206d6c9
Spencer Transier <spencer@transier.us>
2023-09-15 11:56:10 -0700

    Remove unneeded version_helper and test file

--- spec/version_helper_spec.rb
+++
@@ -1,3 +0,0 @@
-require 'spec_helper'
-require_relative '../lib/fastlane/plugin/wpmreleasetoolkit/helper/version_helper'
-require_relative '../lib/fastlane/plugin/wpmreleasetoolkit/models/app_version'
