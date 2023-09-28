0f4a0299003f393baa9ad1ddd1bafa6d99f28208
Spencer Transier <spencer@transier.us>
2023-09-28 14:56:39 -0700

    Rename integer build code to simple build code

--- lib/fastlane/plugin/wpmreleasetoolkit/versioning/formatters/simple_build_code_formatter.rb
+++ lib/fastlane/plugin/wpmreleasetoolkit/versioning/formatters/simple_build_code_formatter.rb
@@ -3,7 +3,7 @@ module Fastlane
     module Versioning
       # The `IntegerBuildCodeFormatter` is a specialized build code formatter for apps that use simple
       # integer build codes in the format of `build_number`.
-      class IntegerBuildCodeFormatter
+      class SimpleBuildCodeFormatter
         # @param version [AppVersion] An AppVersion object. This parameter is ignored but is included
         # to have a consistent signature with other build code formatters.
         #
