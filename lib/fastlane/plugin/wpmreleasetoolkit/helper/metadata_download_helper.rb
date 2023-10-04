fe9a93bc15a06b54e9614ed78167a9e6d328a52a
Gio Lodi <gio@mokacoding.com>
2023-10-04 17:05:04 +1100

    Remove useless parameters from extracted `GlotPressDownloader`

--- lib/fastlane/plugin/wpmreleasetoolkit/helper/metadata_download_helper.rb
+++ lib/fastlane/plugin/wpmreleasetoolkit/helper/metadata_download_helper.rb
@@ -12,27 +12,24 @@ module Fastlane
         @auto_retry_attempt_counter = 0
       end
 
-      def download(target_locale, glotpress_url, is_source)
+      def download(glotpress_url)
         uri = URI(glotpress_url)
         response = Net::HTTP.get_response(uri)
 
         case response.code
-        when '200'
-          # All good pass the result along
+        when '200' # All good pass the result along
           response
-        when '301'
-          # Follow the redirect
-          UI.message("Received 301 for `#{locale}`. Following redirect...")
-          download(locale, response.header['location'], is_source)
-        when '429'
-          # We got rate-limited, auto_retry or offer to try again with a prompt
+        when '301' # Follow the redirect
+          UI.message("Received 301 for `#{response.uri}`. Following redirect...")
+          download(response.header['location'])
+        when '429' # We got rate-limited, auto_retry or offer to try again with a prompt
           if @auto_retry && @auto_retry_attempt_counter <= MAX_AUTO_RETRY_ATTEMPTS
             UI.message("Received 429 for `#{response.uri}`. Auto retrying in #{AUTO_RETRY_SLEEP_TIME} seconds...")
             sleep(AUTO_RETRY_SLEEP_TIME)
             @auto_retry_attempt_counter += 1
-            download(target_locale, response.uri, is_source)
+            download(response.uri)
           elsif UI.confirm("Retry downloading `#{response.uri}` after receiving 429 from the API?")
-            download(target_locale, response.uri, is_source)
+            download(response.uri)
           else
             UI.error("Abandoning `#{response.uri}` download as requested.")
           end
@@ -64,7 +61,7 @@ module Fastlane
       # Downloads data from GlotPress, in JSON format
       def download(target_locale, glotpress_url, is_source)
         downloader = GlotPressDownloader.new(@auto_retry)
-        response = downloader.download(target_locale, glotpress_url, is_source)
+        response = downloader.download(glotpress_url)
         handle_glotpress_download(response: response, locale: target_locale, is_source: is_source)
       end
 
