843381002efe3811a24b3b594d2df3bcdc5ec6e0
Gio Lodi <gio@mokacoding.com>
2023-10-05 14:08:08 +1100

    Add test for GlotPress 429 with manual retry

--- spec/glotpress_downloader_spec.rb
+++ spec/glotpress_downloader_spec.rb
@@ -55,5 +55,30 @@ describe Fastlane::Helper::GlotpressDownloader do
         end
       end
     end
+
+    context 'when auto retry is disabled' do
+      context 'when GlotPress returs a 429 code' do
+        it 'prompt the user for confirmation, ignoring the max auto retry parameter' do
+          sleep_time = 0.1
+          max_retries = 2
+          downloader = described_class.new(auto_retry: false, auto_retry_sleep_time: sleep_time, auto_retry_max_attempts: max_retries)
+          fake_url = 'https://test.com'
+
+          stub_request(:get, fake_url).to_return(status: 429, body: 'Too Many Requests')
+
+          count = 0
+          allow(Fastlane::UI).to receive(:confirm).with("Retry downloading `#{fake_url}` after receiving 429 from the API?") do
+            count += 1
+            # Simulate user retrying more times that the max retries, then aborting
+            count <= max_retries
+          end
+
+          # When the user aborts, with finish with an error
+          expect(Fastlane::UI).to receive(:error).with(/Abandoning `#{fake_url}` download as requested./)
+
+          downloader.download(fake_url)
+        end
+      end
+    end
   end
 end
