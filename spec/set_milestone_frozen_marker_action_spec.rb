e1dc47c4494b3045d439d018478a69ef57a83bf5
Olivier Halligon <olivier.halligon@automattic.com>
2024-02-27 16:43:45 +0100

    DRY some stubs in specs

--- spec/set_milestone_frozen_marker_action_spec.rb
+++ spec/set_milestone_frozen_marker_action_spec.rb
@@ -20,11 +20,11 @@ describe Fastlane::Actions::SetMilestoneFrozenMarkerAction do
 
   before do
     allow(Octokit::Client).to receive(:new).and_return(client)
+    allow(client).to receive(:list_milestones).with(repo).and_return(test_milestones)
   end
 
   context 'when passing `freeze: true` (default)' do
     it 'adds the frozen marker if not present already' do
-      allow(client).to receive(:list_milestones).with(repo).and_return(test_milestones)
       expect(client).to receive(:update_milestone).with(repo, 980, { title: '9.8 Some Custom Title ❄️' })
 
       run_described_fastlane_action(
@@ -36,7 +36,6 @@ describe Fastlane::Actions::SetMilestoneFrozenMarkerAction do
     end
 
     it 'does not add the frozen marker if already present' do
-      allow(client).to receive(:list_milestones).with(repo).and_return(test_milestones)
       expect(client).not_to receive(:update_milestone)
       expect(Fastlane::UI).to receive(:message).with('Logged in as: test')
       expect(Fastlane::UI).to receive(:message).with('Milestone `10.0 The Big Update ❄️` is already frozen. Nothing to do')
@@ -52,7 +51,6 @@ describe Fastlane::Actions::SetMilestoneFrozenMarkerAction do
 
   context 'when passing `freeze: false`' do
     it 'removes the frozen marker if present' do
-      allow(client).to receive(:list_milestones).with(repo).and_return(test_milestones)
       expect(client).to receive(:update_milestone).with(repo, 1000, { title: '10.0 The Big Update' })
 
       run_described_fastlane_action(
@@ -64,7 +62,6 @@ describe Fastlane::Actions::SetMilestoneFrozenMarkerAction do
     end
 
     it 'does nothing when trying to remove the marker while not present' do
-      allow(client).to receive(:list_milestones).with(repo).and_return(test_milestones)
       expect(client).not_to receive(:update_milestone)
       expect(Fastlane::UI).to receive(:message).with('Logged in as: test')
       expect(Fastlane::UI).to receive(:message).with('Milestone `9.8 Some Custom Title` is not frozen. Nothing to do')
