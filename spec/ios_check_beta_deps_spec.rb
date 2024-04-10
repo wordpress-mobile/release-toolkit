dc5992fd2fbe7e4459b134ce6b1f82805254635a
Olivier Halligon <olivier.halligon@automattic.com>
2024-04-11 00:51:55 +0200

    Red: Tests for `Podfile.lock` w/o external sources

--- spec/ios_check_beta_deps_spec.rb
+++ spec/ios_check_beta_deps_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 
 describe Fastlane::Actions::IosCheckBetaDepsAction do
-  let(:lockfile_fixture_path) { File.join(File.dirname(__FILE__), 'test-data', 'Podfile.lock') }
+  let(:lockfile_with_external_sources) { File.join(File.dirname(__FILE__), 'test-data', 'Podfile-with-external-sources.lock') }
+  let(:lockfile_without_external_sources) { File.join(File.dirname(__FILE__), 'test-data', 'Podfile-without-external-sources.lock') }
 
   def violation_message(**pods)
     list = pods.map do |pod, reason|
@@ -25,7 +26,7 @@ describe Fastlane::Actions::IosCheckBetaDepsAction do
     expect(FastlaneCore::UI).to receive(:important).with(expected_message)
 
     result = run_described_fastlane_action(
-      lockfile: lockfile_fixture_path
+      lockfile: lockfile_with_external_sources
     )
 
     expect(result[:pods]).to eq(expected_violations)
@@ -42,7 +43,7 @@ describe Fastlane::Actions::IosCheckBetaDepsAction do
     expect(FastlaneCore::UI).to receive(:important).with(expected_message)
 
     result = run_described_fastlane_action(
-      lockfile: lockfile_fixture_path,
+      lockfile: lockfile_with_external_sources,
       report_commits: false
     )
 
@@ -60,7 +61,7 @@ describe Fastlane::Actions::IosCheckBetaDepsAction do
     expect(FastlaneCore::UI).to receive(:important).with(expected_message)
 
     result = run_described_fastlane_action(
-      lockfile: lockfile_fixture_path,
+      lockfile: lockfile_with_external_sources,
       report_branches: false
     )
 
@@ -77,7 +78,7 @@ describe Fastlane::Actions::IosCheckBetaDepsAction do
     expect(FastlaneCore::UI).to receive(:important).with(expected_message)
 
     result = run_described_fastlane_action(
-      lockfile: lockfile_fixture_path,
+      lockfile: lockfile_with_external_sources,
       report_version_pattern: ''
     )
 
@@ -95,7 +96,7 @@ describe Fastlane::Actions::IosCheckBetaDepsAction do
     expect(FastlaneCore::UI).to receive(:important).with(expected_message)
 
     result = run_described_fastlane_action(
-      lockfile: lockfile_fixture_path,
+      lockfile: lockfile_with_external_sources,
       report_version_pattern: '.*-rc-\d'
     )
 
@@ -108,7 +109,7 @@ describe Fastlane::Actions::IosCheckBetaDepsAction do
     expect(FastlaneCore::UI).to receive(:important).with(expected_message)
 
     result = run_described_fastlane_action(
-      lockfile: lockfile_fixture_path,
+      lockfile: lockfile_with_external_sources,
       report_commits: false,
       report_branches: false,
       report_version_pattern: ''
@@ -118,10 +119,26 @@ describe Fastlane::Actions::IosCheckBetaDepsAction do
     expect(result[:message]).to eq(expected_message)
   end
 
+  it 'does not report any error if all pods resolve to stable versions' do
+    expected_message = Fastlane::Actions::IosCheckBetaDepsAction::ALL_PODS_STABLE_MESSAGE
+    expect(FastlaneCore::UI).to receive(:important).with(expected_message)
+
+    # Note how, in `Podfile-without-external-sources.lock`, pods like `WordPressAuthenticator`
+    # do have a *dependency constraint* on `WordPressShared (~> 2.1-beta)`, but `WordPressShared`
+    # ends up being resolved to `2.3.1` so this is ok. This test thus also ensure that we do
+    # NOT flag `(~> *-beta)` strings found in constraints, but only from *resolved* versions.
+    result = run_described_fastlane_action(
+      lockfile: lockfile_without_external_sources
+    )
+
+    expect(result[:pods]).to eq({})
+    expect(result[:message]).to eq(expected_message)
+  end
+
   it 'raises user_error! if regex is invalid' do
     expect do
       run_described_fastlane_action(
-        lockfile: lockfile_fixture_path,
+        lockfile: lockfile_with_external_sources,
         report_version_pattern: '*-rc-\d'
       )
     end.to raise_exception(FastlaneCore::Interface::FastlaneError, 'Invalid regex pattern: `*-rc-\d`')
