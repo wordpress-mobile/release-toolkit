2a8515cac566a5e0132d8bc1af0b9b92497afb33
Gio Lodi <gio@mokacoding.com>
2024-04-24 12:39:47 +1000

    Use RSpec `double` instead of custom `FakeResponse`
    Co-authored-by: Olivier Halligon <olivier.halligon@automattic.com>
    
    Notice that this implementation is not ideal as far as RuboCop is
    concerned.
    
    The recommendation to use a verified double (`instance_double`) comes
    with the intent of improving the test correctness by ensuring that the
    expected object type is used in the system under test.
    
    While that is an excellent general recommendation, in this particular
    instance it backfires because the object `Buildkit` returns when
    creating a build is `Sawyer::Resource` which we cannot as easily mock.
    
    I.e., mocking with
    
    ```ruby
    .and_return(
      instance_double(
        Sawyer::Resource,
        state: 'scheduled',
        web_url: expected_url
      )
    )
    ```
    
    Fails with:
    
    ```
    the Sawyer::Resource class does not implement the instance method: state
    ```
    
    I also tried to create a dummy `Sawyer::Resource` instance, but that
    required providing a client object.
    
    If this implementation is not satisfactory, we could consider stubbing
    the Buildkite API HTTP response. I didn't go down that route when first
    working on this because I didn't want to go look for endpoints and JSON
    responses, but it's an option worth considering.

--- spec/buildkite_trigger_build_action_spec.rb
+++ spec/buildkite_trigger_build_action_spec.rb
@@ -1,8 +1,6 @@
 require 'spec_helper'
 require 'buildkit'
 
-FakeResponse = Struct.new(:state, :web_url)
-
 describe Fastlane::Actions::BuildkiteTriggerBuildAction do
   let(:buildkit) { Buildkit.new(token: 'test') }
 
@@ -13,8 +11,14 @@ describe Fastlane::Actions::BuildkiteTriggerBuildAction do
   context 'when the API responds with state scheduled' do
     it 'returns the new build web URL' do
       expected_url = 'https://fake.url'
+      # RuboCop recommends using instance_double, but doing so requires digging into Sawyer::Resource.
+      # That object uses metaprogramming to generate accessors for the keys in the JSON response that created it.
+      # For the sake of keeping the test simple, let's use a non-verifying double.
+      #
+      # rubocop:disable RSpec/VerifiedDoubles
       allow(buildkit).to receive(:create_build)
-        .and_return(FakeResponse.new('scheduled', expected_url))
+        .and_return(double(state: 'scheduled', web_url: expected_url))
+      # rubocop:enable RSpec/VerifiedDoubles
 
       url = run_described_fastlane_action(
         branch: 'branch',
@@ -30,8 +34,10 @@ describe Fastlane::Actions::BuildkiteTriggerBuildAction do
 
   context 'when the API responds with a state other than scheduled' do
     it 'raises an error' do
+      # rubocop:disable RSpec/VerifiedDoubles
       allow(buildkit).to receive(:create_build)
-        .and_return(FakeResponse.new('fail', 'this-is-ignored'))
+        .and_return(double(state: 'fail', web_url: 'this-is-ignored'))
+      # rubocop:enable RSpec/VerifiedDoubles
 
       # Don't care about the error message
       expect(FastlaneCore::UI).to receive(:crash!)
