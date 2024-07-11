17a7337c65d8204f8230f35c1dd64cff08c08027
Ian Maia <ian.maia@automattic.com>
2024-07-11 13:34:42 +0200

    Add Action to create release backmerge pull requests

---
+++ lib/fastlane/plugin/wpmreleasetoolkit/actions/common/create_release_backmerge_pull_request_action.rb
@@ -0,0 +1,163 @@
+require 'fastlane/action'
+require_relative '../../helper/github_helper'
+
+module Fastlane
+  module Actions
+    class CreateReleaseBackmergePullRequestAction < Action
+      DEFAULT_BRANCH = 'trunk'.freeze
+
+      def self.run(params)
+        token = params[:github_token]
+        repository = params[:repository]
+        release_branch = params[:release_branch]
+        default_branch = params[:default_branch]
+        target_branches_param = params[:target_branches]
+        labels = params[:labels]
+        milestone_title = params[:milestone_title]
+
+        unless release_branch.start_with?('release/')
+          UI.user_error!('`release_branch` must start with `release/`')
+        end
+
+        if target_branches_param.include?(release_branch)
+          UI.user_error!('`target_branches` must not contain `release_branch`')
+        end
+
+        github_helper = Fastlane::Helper::GithubHelper.new(github_token: params[:github_token])
+        target_milestone = milestone_title.nil? ? nil : github_helper.get_milestone(repository, milestone_title)
+
+        target_branches = if target_branches_param.empty?
+                            determine_target_branches(release_branch.delete('release/'), default_branch)
+                          else
+                            target_branches_param
+                          end
+
+        pr_urls = []
+
+        target_branches.each do |target_branch|
+          Fastlane::Helper::GitHelper.checkout_and_pull(release_branch)
+
+          pr_urls << create_backmerge_pr(
+            token: token,
+            repository: params[:repository],
+            title: "Merge #{release_branch} into #{target_branch}",
+            head_branch: release_branch,
+            base_branch: target_branch,
+            milestone: target_milestone&.number,
+            labels: labels
+          )
+        end
+
+        pr_urls
+      end
+
+      def self.determine_target_branches(release_version, default_branch)
+        release_branches = Actions.sh('git branch -r -l "origin/release/*"').chomp
+
+        all_release_branches_versions = release_branches
+                                        .split("\n")
+                                        .map { |branch| branch.match(%r{origin/release/([0-9.]*)})&.captures&.first }
+                                        .compact
+
+        target_branches = all_release_branches_versions.select { |branch| Gem::Version.new(branch) > Gem::Version.new(release_version) }
+                                                       .map { |v| "release/#{v}" }
+        target_branches = [default_branch] if target_branches.empty?
+
+        target_branches
+      end
+
+      def self.create_backmerge_pr(token:, repository:, title:, head_branch:, base_branch:, milestone:, labels:)
+        intermediate_branch = "merge/#{head_branch.gsub('/', '-')}-into-#{base_branch.gsub('/', '-')}"
+        Fastlane::Helper::GitHelper.create_branch(intermediate_branch)
+
+        other_action.push_to_git_remote(tags: false)
+
+        pr_body = <<~BODY
+          Merging `#{head_branch}` into `#{base_branch}`.
+
+          Via intermediate branch `#{intermediate_branch}`, to help fix conflicts if any:
+          ```
+          #{head_branch.rjust(40)}  ----o-- - - -
+          #{' ' * 40}       \\
+          #{intermediate_branch.rjust(40)}        `---.
+          #{' ' * 40}             \\
+          #{base_branch.rjust(40)}  ------------x- - -
+          ```
+        BODY
+
+        other_action.create_pull_request(
+          api_token: token,
+          repo: repository,
+          title: title,
+          body: pr_body,
+          head: intermediate_branch,
+          base: base_branch,
+          labels: labels,
+          milestone: milestone
+        )
+      end
+
+      def self.description
+        'Creates backmerge PRs for a release branch into target branches'
+      end
+
+      def self.authors
+        ['Automattic']
+      end
+
+      def self.return_type
+        :array_of_strings
+      end
+
+      def self.return_value
+        'The list of backmerge PRs created'
+      end
+
+      def self.details
+        <<~DETAILS
+          This action creates backmerge Pull Requests from a release branch into one or more target branches.
+
+          It can be used to ensure that changes from a release branch are merged back into other branches, such as newer release branches or the main development branch (e.g., `trunk`).
+        DETAILS
+      end
+
+      def self.available_options
+        [
+          FastlaneCore::ConfigItem.new(key: :repository,
+                                       env_name: 'GHHELPER_REPOSITORY',
+                                       description: 'The remote path of the GH repository on which we work',
+                                       optional: false,
+                                       type: String),
+          FastlaneCore::ConfigItem.new(key: :release_branch,
+                                       description: 'The release branch, in the format `release/x.y.z`',
+                                       optional: false,
+                                       type: String),
+          FastlaneCore::ConfigItem.new(key: :default_branch,
+                                       description: 'The default branch to target if no newer release branches exist',
+                                       optional: true,
+                                       default_value: DEFAULT_BRANCH,
+                                       type: String),
+          FastlaneCore::ConfigItem.new(key: :target_branches,
+                                       description: 'Array of target branches for the backmerge. If empty, the action will determine target branches by finding all `release/x.y.z` branches with a version greater than `release_version`. If none are found, it will target `default_branch`',
+                                       optional: true,
+                                       default_value: [],
+                                       type: Array),
+          FastlaneCore::ConfigItem.new(key: :labels,
+                                       description: 'The labels that should be assigned to the backmerge PRs',
+                                       optional: true,
+                                       default_value: [],
+                                       type: Array),
+          FastlaneCore::ConfigItem.new(key: :milestone_title,
+                                       description: 'The title of the milestone to assign to the created PRs',
+                                       optional: true,
+                                       type: String),
+          Fastlane::Helper::GithubHelper.github_token_config_item,
+        ]
+      end
+
+      def self.is_supported?(platform)
+        true
+      end
+    end
+  end
+end
---
+++ spec/create_release_backmerge_pull_request_spec.rb
@@ -0,0 +1,236 @@
+require 'spec_helper'
+
+describe Fastlane::Actions::CreateReleaseBackmergePullRequestAction do
+  let(:test_token) { 'ghp_fake_token' }
+  let(:test_repo) { 'repo-test/project-test' }
+  let(:default_branch) { 'main' }
+  let(:other_action_mock) { double }
+  let(:client) do
+    instance_double(
+      Octokit::Client,
+      user: instance_double('User', name: 'test'),
+      list_milestones: %w[30.7 30.6 30.5 30.5.1 30.4].map { |version| mock_milestone(version) },
+      'auto_paginate=': nil
+    )
+  end
+
+  def mock_milestone(release_branch)
+    release_version = release_branch.delete('release/')
+    number = release_version.gsub('.', '').to_i
+    sawyer_resource_stub(title: release_version, number: number)
+  end
+
+  def mock_pr_url(release_branch)
+    number = release_branch.delete('release/').gsub('.', '').to_i
+    "https://github.com/#{test_repo}/pull/#{number}"
+  end
+
+  def stub_git_release_branches(branches)
+    allow(Fastlane::Actions).to receive(:sh)
+      .with('git branch -r -l "origin/release/*"')
+      .and_return("\n" + branches.map { |release| "origin/#{release}" }.join("\n") + "\n")
+  end
+
+  def stub_expected_pull_requests(expected_backmerge_branches:, release_branch:, labels: [], milestone_number: nil)
+    expected_backmerge_branches.each do |target_branch|
+      expected_intermediate_branch = "merge/#{release_branch.gsub('/', '-')}-into-#{target_branch.gsub('/', '-')}"
+
+      expect(Fastlane::Helper::GitHelper).to receive(:checkout_and_pull).with(release_branch)
+      expect(Fastlane::Helper::GitHelper).to receive(:create_branch).with(expected_intermediate_branch)
+      expect(other_action_mock).to receive(:push_to_git_remote).with(tags: false)
+      allow(other_action_mock).to receive(:create_pull_request).with(
+        api_token: test_token,
+        repo: test_repo,
+        title: "Merge #{release_branch} into #{target_branch}",
+        body: anything,
+        head: expected_intermediate_branch,
+        base: target_branch,
+        labels: labels,
+        milestone: milestone_number
+      ).and_return(mock_pr_url(target_branch))
+    end
+  end
+
+  before do
+    allow(Octokit::Client).to receive(:new).and_return(client)
+    allow(Fastlane::Helper::GitHelper).to receive(:checkout_and_pull)
+    allow(Fastlane::Helper::GitHelper).to receive(:create_branch)
+    allow(Fastlane::Action).to receive(:other_action).and_return(other_action_mock)
+  end
+
+  context 'when `target_branches` is provided' do
+    it 'creates a backmerge PR for each target branch' do
+      # all release branches should be ignored as we're providing `target_branches`
+      stub_git_release_branches(%w[release/30.5 release/30.6 release/30.7 release/30.8])
+
+      release_branch = 'release/30.7'
+
+      expected_backmerge_branches = %w[trunk release/30.6]
+      stub_expected_pull_requests(
+        expected_backmerge_branches: expected_backmerge_branches,
+        release_branch: release_branch
+      )
+
+      result = run_described_fastlane_action(
+        github_token: test_token,
+        repository: test_repo,
+        release_branch: release_branch,
+        target_branches: expected_backmerge_branches
+      )
+
+      expect(result).to eq(expected_backmerge_branches.map { |target_branch| mock_pr_url(target_branch) })
+    end
+  end
+
+  context 'when `target_branches` is not provided' do
+    it 'determines target branches and creates backmerge PRs' do
+      # only release branches with version > `release_branch` should have a backmerge created
+      stub_git_release_branches(%w[release/30.6 release/30.5.1 release/30.5 release/30.4])
+
+      release_branch = 'release/30.5'
+
+      expected_backmerge_branches = %w[release/30.6 release/30.5.1]
+      stub_expected_pull_requests(
+        expected_backmerge_branches: expected_backmerge_branches,
+        release_branch: release_branch
+      )
+
+      result = run_described_fastlane_action(
+        github_token: test_token,
+        repository: test_repo,
+        release_branch: release_branch
+      )
+
+      expect(result).to eq(expected_backmerge_branches.map { |target_branch| mock_pr_url(target_branch) })
+    end
+
+    it 'defaults to the `default_branch` if no newer release branches are found' do
+      stub_git_release_branches(%w[release/30.6 release/30.5])
+
+      release_branch = 'release/30.6'
+
+      stub_expected_pull_requests(
+        expected_backmerge_branches: [default_branch],
+        release_branch: release_branch
+      )
+
+      result = run_described_fastlane_action(
+        github_token: test_token,
+        repository: test_repo,
+        release_branch: release_branch,
+        default_branch: default_branch
+      )
+
+      expect(result).to eq([mock_pr_url(default_branch)])
+    end
+  end
+
+  context 'when providing invalid input' do
+    it 'throws an error when the `release_branch` is invalid' do
+      stub_git_release_branches([])
+
+      release_branch = '30.6'
+
+      expect do
+        run_described_fastlane_action(
+          github_token: test_token,
+          repository: test_repo,
+          release_branch: release_branch,
+          default_branch: default_branch
+        )
+      end.to raise_error(FastlaneCore::Interface::FastlaneError, '`release_branch` must start with `release/`')
+    end
+
+    it 'uses the `release_branch` in one of the `target_branches`' do
+      stub_git_release_branches([])
+
+      release_branch = 'release/30.6'
+
+      expect do
+        run_described_fastlane_action(
+          github_token: test_token,
+          repository: test_repo,
+          release_branch: release_branch,
+          default_branch: default_branch,
+          target_branches: ['release/30.6.1', 'release/30.6', 'release/30.7']
+        )
+      end.to raise_error(FastlaneCore::Interface::FastlaneError, '`target_branches` must not contain `release_branch`')
+    end
+  end
+
+  context 'when providing a milestone' do
+    it 'creates a backmerge PR setting a milestone' do
+      stub_git_release_branches([])
+
+      release_branch = 'release/30.6'
+      milestone = mock_milestone(release_branch)
+
+      stub_expected_pull_requests(
+        expected_backmerge_branches: [default_branch],
+        release_branch: release_branch,
+        milestone_number: milestone[:number]
+      )
+
+      result = run_described_fastlane_action(
+        github_token: test_token,
+        repository: test_repo,
+        release_branch: release_branch,
+        default_branch: default_branch,
+        milestone_title: milestone[:title]
+      )
+
+      expect(result).to eq([mock_pr_url(default_branch)])
+    end
+
+    it 'no milestone is set if an unknown milestone is used' do
+      stub_git_release_branches([])
+
+      release_branch = 'release/30.7'
+
+      stub_expected_pull_requests(
+        expected_backmerge_branches: [default_branch],
+        release_branch: release_branch,
+        milestone_number: nil
+      )
+
+      result = run_described_fastlane_action(
+        github_token: test_token,
+        repository: test_repo,
+        release_branch: release_branch,
+        default_branch: default_branch,
+        milestone_title: 'nonexistentmilestone'
+      )
+
+      expect(result).to eq([mock_pr_url(default_branch)])
+    end
+  end
+
+  context 'when providing labels' do
+    [
+      %w[java ruby perl],
+      [],
+    ].each do |labels|
+      it "creates a backmerge PR setting the labels: #{labels}" do
+        stub_git_release_branches([])
+
+        release_branch = 'release/30.6'
+
+        stub_expected_pull_requests(
+          expected_backmerge_branches: [default_branch],
+          release_branch: release_branch,
+          labels: labels
+        )
+
+        result = run_described_fastlane_action(
+          github_token: test_token,
+          repository: test_repo,
+          release_branch: release_branch,
+          default_branch: default_branch,
+          labels: labels
+        )
+
+        expect(result).to eq([mock_pr_url(default_branch)])
+      end
+    end
+  end
+end
