80dc73e8484a243678e17aa80056cab6f3912394
Gio Lodi <gio@mokacoding.com>
2024-08-06 17:09:59 +1000

    Remove some unused `calc_..._version` methods

--- lib/fastlane/plugin/wpmreleasetoolkit/helper/android/android_version_helper.rb
+++ lib/fastlane/plugin/wpmreleasetoolkit/helper/android/android_version_helper.rb
@@ -73,45 +73,6 @@ module Fastlane
           name.nil? || code.nil? ? nil : { VERSION_NAME => name, VERSION_CODE => code.to_i }
         end
 
-        # Returns the version name and code to use for the final release.
-        #
-        # - The final version name corresponds to the beta's versionName, without the `-rc` suffix
-        # - The final version code corresponds to the versionCode for the alpha (or for the beta if alpha_version is nil) incremented by one.
-        #
-        # @param [Hash] beta_version The version hash for the beta, containing values for keys "name" and "code"
-        # @param [Hash] alpha_version The version hash for the alpha, containing values for keys "name" and "code",
-        #                             or `nil` if no alpha version to consider.
-        #
-        # @return [Hash] A version hash with keys "name" and "code", containing the version name and code to use for final release.
-        #
-        def self.calc_final_release_version(beta_version, alpha_version)
-          version_name = beta_version[VERSION_NAME].split('-')[0]
-          version_code = alpha_version.nil? ? beta_version[VERSION_CODE] + 1 : alpha_version[VERSION_CODE] + 1
-
-          { VERSION_NAME => version_name, VERSION_CODE => version_code }
-        end
-
-        # Returns the version name and code to use for the next alpha.
-        #
-        # - The next version name corresponds to the `alpha_version`'s name incremented by one (alpha-42 => alpha-43)
-        # - The next version code corresponds to the `version`'s code incremented by one.
-        #
-        # @param [Hash] version The version hash for the current beta or release, containing values for keys "name" and "code"
-        # @param [Hash] alpha_version The version hash for the current alpha (defaultConfig), containing values for keys "name" and "code"
-        #
-        # @return [Hash] A version hash with keys "name" and "code", containing the version name and code to use for final release.
-        #
-        def self.calc_next_alpha_version(version, alpha_version)
-          # Bump alpha name
-          alpha_number = alpha_version[VERSION_NAME].sub(ALPHA_PREFIX, '')
-          alpha_name = "#{ALPHA_PREFIX}#{alpha_number.to_i + 1}"
-
-          # Bump alpha code
-          alpha_code = version[VERSION_CODE] + 1
-
-          { VERSION_NAME => alpha_name, VERSION_CODE => alpha_code }
-        end
-
         # Compute the version name and code to use for the next beta (`X.Y.Z-rc-N`).
         #
         # - The next version name corresponds to the `version`'s name with the value after the `-rc-` suffix incremented by one,
@@ -188,40 +149,6 @@ module Fastlane
           calc_next_beta_version(nv)
         end
 
-        # Compute the name and code of the next hotfix version.
-        #
-        # @param [String] hotfix_version_name The next version name we want for the hotfix
-        # @param [String] hotfix_version_code The next version code we want for the hotfix
-        #
-        # @return [Hash] The predicted next hotfix version, as a Hash containing the keys `"name"` and `"code"`
-        #
-        def self.calc_next_hotfix_version(hotfix_version_name, hotfix_version_code)
-          { VERSION_NAME => hotfix_version_name, VERSION_CODE => hotfix_version_code }
-        end
-
-        # Compute the name of the previous release version, by decrementing the minor version number
-        #
-        # @example
-        #    calc_prev_release_version("1.2") => "1.1"
-        #    calc_prev_release_version("1.2.3") => "1.1"
-        #    calc_prev_release_version("3.0") => "2.9"
-        #
-        # @param [String] version The version string to decrement
-        #
-        # @return [String] A 2-parts version string "X.Y" corresponding to the guessed previous release version.
-        #
-        def self.calc_prev_release_version(version)
-          vp = get_version_parts(version)
-          if (vp[MINOR_NUMBER]).zero?
-            vp[MAJOR_NUMBER] -= 1
-            vp[MINOR_NUMBER] = 9
-          else
-            vp[MINOR_NUMBER] -= 1
-          end
-
-          "#{vp[MAJOR_NUMBER]}.#{vp[MINOR_NUMBER]}"
-        end
-
         # Determines if a version name corresponds to a hotfix
         #
         # @param [String] version The version number to test
@@ -259,21 +186,6 @@ module Fastlane
             update_version(new_version_beta, 'defaultConfig')
             update_version(new_version_alpha, 'defaultConfig') unless new_version_alpha.nil?
           end
-        end
-
-        # Compute the name of the previous hotfix version.
-        #
-        # @param [String] version_name The current version name we want to decrement
-        #
-        # @return [String] The predicted previous hotfix version, in the form of "X.Y.Z", or "X.Y" if Z is 0.
-        #         Corresponds to decrementing the 3rd component Z of the version, stripping it if it ends up being zero.
-        #
-        def self.calc_prev_hotfix_version_name(version_name)
-          vp = get_version_parts(version_name)
-          vp[HOTFIX_NUMBER] -= 1 unless (vp[HOTFIX_NUMBER]).zero?
-          return "#{vp[MAJOR_NUMBER]}.#{vp[MINOR_NUMBER]}.#{vp[HOTFIX_NUMBER]}" unless (vp[HOTFIX_NUMBER]).zero?
-
-          "#{vp[MAJOR_NUMBER]}.#{vp[MINOR_NUMBER]}"
         end
 
         # Extract the value of a import key from build.gradle
