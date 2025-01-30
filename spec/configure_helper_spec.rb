952f393e5df5b1b1389aae37ac3c58ab620b0c96
Gio Lodi <gio@mokacoding.com>
2025-01-30 16:18:57 +1100

    Add integration tests for `configure_helper` `repo_branch_name`

--- spec/configure_helper_spec.rb
+++ spec/configure_helper_spec.rb
@@ -1,8 +1,48 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
+require 'tmpdir'
 
 describe Fastlane::Helper::ConfigureHelper do
+  let(:test_repo_path) { Dir.mktmpdir }
+
+  def run_git_command(command:, repo_path: test_repo_path)
+    # Notice that this will break if the command contains arguments in quotes,
+    # e.g. `commit -m "Test commit"` will be split into `['commit', '-m', '"Test', 'commit"']`
+    # which is not a valid arguments list for Git.
+    #
+    # For the context of these tests, we can deal with this limitation.
+    system('git', '-C', repo_path, *command.split, %I[out err] => File::NULL)
+  end
+
+  before do
+    allow(described_class).to receive(:repository_path).and_return(test_repo_path)
+
+    run_git_command(command: 'init')
+    run_git_command(command: 'config user.email test@example.com')
+    run_git_command(command: 'config user.name Test User')
+    File.write(File.join(test_repo_path, 'test.txt'), 'test content')
+    run_git_command(command: 'add test.txt')
+    run_git_command(command: 'commit -m test')
+  end
+
+  after do
+    FileUtils.remove_entry test_repo_path
+  end
+
+  describe '.repo_branch_name' do
+    it 'returns the current branch name when on a branch' do
+      run_git_command(command: 'checkout -b feature-branch')
+      expect(described_class.repo_branch_name).to eq('feature-branch')
+    end
+
+    it 'returns nil when in detached HEAD state' do
+      current_sha = `git -C #{test_repo_path} rev-parse HEAD`.strip
+      run_git_command(command: "checkout #{current_sha}")
+      expect(described_class.repo_branch_name).to be_nil
+    end
+  end
+
   describe '#add_file' do
     let(:destination) { 'path/to/destination' }
 
